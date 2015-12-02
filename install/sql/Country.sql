9288502991787c80a4c02310f127c88da6866806
Dominik Pfaffenbauer <dominik@lineofcode.at>
2015-12-02 09:44:23 +0100

    fix install SQL, set country column to name column

--- install/sql/Country.sql
+++ install/sql/Country.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `coreshop_country` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `country` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `isoCode` varchar(2) NULL,
   `active` int(1) DEFAULT 0,
   `currency__id` int(1),
