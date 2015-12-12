59d21a819da26ef76be8901d7f6223ca436552e2
Dominik Pfaffenbauer <dominik@lineofcode.at>
2015-12-12 18:09:22 +0100

    fix snippet and controller errors in editmode

--- lib/CoreShop/Controller/Action.php
+++ lib/CoreShop/Controller/Action.php
@@ -40,7 +40,6 @@ class Action extends \Website\Controller\Action {
                 $this->view->getScriptPaths(),
                 array(
                     CORESHOP_TEMPLATE_PATH . '/views/scripts/',
-                    CORESHOP_TEMPLATE_PATH . '/views/scripts/',
                     CORESHOP_TEMPLATE_PATH . '/views/layouts/',
                     CORESHOP_TEMPLATE_PATH . '/views/scripts/coreshop/'
                 )
