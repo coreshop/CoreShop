5b498e3c8dec22063f1a32a2ad887158c266802a
Dominik Pfaffenbauer <dominik@lineofcode.at>
2016-02-18 12:36:48 +0100

    change name of permissions

--- install/sql/CoreShop.sql
+++ install/sql/CoreShop.sql
@@ -474,17 +474,19 @@ CREATE TABLE `coreshop_customer_groups` (
 
 INSERT INTO `users_permission_definitions` (`key`)
 VALUES
-  ('coreshop_permission_carrier'),
-  ('coreshop_permission_zone'),
+  ('coreshop_permission_carriers'),
+  ('coreshop_permission_zones'),
   ('coreshop_permission_settings'),
   ('coreshop_permission_priceRules'),
   ('coreshop_permission_orderStates'),
-  ('coreshop_permission_currency'),
+  ('coreshop_permission_currencies'),
   ('coreshop_permission_taxes'),
   ('coreshop_permission_tax_rules'),
   ('coreshop_permission_customer_groups'),
   ('coreshop_permission_plugins'),
-  ('coreshop_permission_country');
+  ('coreshop_permission_countries'),
+  ('coreshop_permission_filters'),
+  ('coreshop_permission_indexes');
 
 DROP TABLE IF EXISTS `coreshop_product_specificprice`;
 CREATE TABLE `coreshop_product_specificprice` (
