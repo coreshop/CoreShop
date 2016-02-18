0d2f7bff758882c9cb24ace7f6e436b04b5dff10
Dominik Pfaffenbauer <dominik@lineofcode.at>
2016-02-18 14:29:11 +0100

    case #74 add command to update-index

---
+++ lib/CoreShop/Console/Command/IndexCommand.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CoreShop
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015 Dominik Pfaffenbauer (http://dominik.pfaffenbauer.at)
+ * @license    http://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Console\Command;
+
+use CoreShop\IndexService;
+use CoreShop\Model\Product;
+use Pimcore\Console\AbstractCommand;
+use Pimcore\Model\Object\CoreShopProduct;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class IndexCommand extends AbstractCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('coreshop-index')
+            ->setDescription('Reindex all Products');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $allProducts = new CoreShopProduct\Listing();
+        $allProducts->setObjectTypes(array(CoreShopProduct::OBJECT_TYPE_OBJECT, CoreShopProduct::OBJECT_TYPE_VARIANT));
+        $allProducts = $allProducts->load();
+
+        $steps = count($allProducts);
+
+        $this->output->writeln("<info>Found ".count($allProducts)." Products to index</info>");
+
+        $progress = new ProgressBar($output, $steps);
+        $progress->start();
+
+        foreach($allProducts as $product) {
+            IndexService::getIndexService()->updateIndex($product);
+            $progress->advance();
+        }
+        $this->output->writeln("");
+        $this->output->writeln("<info>Done</info>");
+    }
+}
