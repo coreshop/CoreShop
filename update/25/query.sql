b9d927a21847a7fb95c3b7f3011ded96ffa628a7
Dominik Pfaffenbauer <dominik@lineofcode.at>
2016-03-07 17:49:20 +0100

    remove deprecated update folder

--- update/25/query.sql
+++
@@ -1,9 +0,0 @@
-
-DROP TABLE IF EXISTS `coreshop_product_specificprice`;
-CREATE TABLE `coreshop_product_specificprice` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `o_id` int NOT NULL,
-  `name` varchar(50) NULL,
-  `conditions` text NULL,
-  `actions` text NULL
-) COMMENT='';
--- update/27/postupdate.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//Set Default Out-of-Stock behaviour to ALLOW_ORDER
-\CoreShop\Model\Configuration::set("SYSTEM.STOCK.DEFAULTOUTOFSTOCKBEHAVIOUR", 1);
