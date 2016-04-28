1b9dc667cbbef2b75f134e788f6c9e139c02df5e
dpfaffenbauer <dominik@lineofcode.at>
2016-04-28 20:24:09 +0200

    revert local php call

--- tests/alltests.sh
+++ tests/alltests.sh
@@ -2,5 +2,5 @@
 
 mkdir -p output
 
-/Applications/MAMP/bin/php/php5.6.10/bin/php phpunit.php --verbose --bootstrap bootstrap.php --log-json output/log.xml AllTests
+php phpunit.php --verbose --bootstrap bootstrap.php --log-json output/log.xml AllTests
 
