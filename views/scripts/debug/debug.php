c535497c0369762a40a5ff6723c0cdca4726c83c
dpfaffenbauer <dominik@lineofcode.at>
2016-08-18 17:01:17 +0100

    extend coreshop-debugger

--- views/scripts/debug/debug.php
+++ views/scripts/debug/debug.php
@@ -12,10 +12,10 @@
         <div class="coreshop-debug-panel-body" style="display:none">
             <table class="coreshop-debug-table">
                 <?php if(\Pimcore\Model\Staticroute::getCurrentRoute() instanceof \Pimcore\Model\Staticroute) { ?>
-                <tr>
-                    <td>Staticroute</td>
-                    <td><?=\Pimcore\Model\Staticroute::getCurrentRoute()->getName()?></td>
-                </tr>
+                    <tr>
+                        <td>Staticroute</td>
+                        <td><?=\Pimcore\Model\Staticroute::getCurrentRoute()->getName()?></td>
+                    </tr>
                 <?php } ?>
                 <?php if(\CoreShop\Model\Configuration::multiShopEnabled()) { ?>
                     <tr>
@@ -32,22 +32,75 @@
                     <td><?=\CoreShop\Tool::getCurrency()->getName() ?> (<?=\CoreShop\Tool::getCurrency()->getId()?>)</td>
                 </tr>
 
+                <?php if(\CoreShop\Tool::getUser() instanceof \CoreShop\Model\User) { ?>
+                    <tr>
+                        <td><?=$this->translate("coreshop_user")?></td>
+                        <td><?=\CoreShop\Tool::getUser()->getEmail() ?> (<?=\CoreShop\Tool::getUser()->getId()?>)</td>
+                    </tr>
+
+                    <?php foreach(\CoreShop\Tool::getUser()->getCustomerGroups() as $group) { ?>
+                        <tr>
+                            <td><?=$this->translate("coreshop_customer_group")?></td>
+                            <td><?=$group->getName() ?> (<?=$group->getId()?>)</td>
+                        </tr>
+                    <?php } ?>
+                <?php } ?>
+
                 <?php if($this->product instanceof \CoreShop\Model\Product) {
                     ?>
                     <tr>
                         <td><?=$this->translate("coreshop_product")?></td>
                         <td><?=$this->product->getName()?> (<?=$this->product->getId()?>)</td>
                     </tr>
-                    <?php if($this->product->getTaxRule() instanceof \CoreShop\Model\TaxRuleGroup) { ?>
                     <tr>
-                        <td><?=$this->translate("coreshop_taxrulegroups")?></td>
-                        <td><?=$this->product->getTaxRule()->getName()?> (<?=$this->product->getTaxRule()->getId()?>)</td>
+                        <td><?=$this->translate("coreshop_retail_price")?></td>
+                        <td><?=\CoreShop\Tool::formatPrice($this->product->getRetailPrice())?></td>
                     </tr>
+                    <tr>
+                        <td><?=$this->translate("coreshop_price")?></td>
+                        <td><?=\CoreShop\Tool::formatPrice($this->product->getPrice(true))?></td>
+                    </tr>
+                    <tr>
+                        <td><?=$this->translate("coreshop_price_rules")?></td>
+                        <td>
+                            <table class="coreshop-debug-table">
+                                <thead>
+                                <tr>
+                                    <td><?=$this->translate("corshop_price_rule_name")?></td>
+                                    <td><?=$this->translate("coreshop_price")?></td>
+                                    <td><?=$this->translate("coreshop_discount")?></td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                $priceRules = $this->product->getValidSpecificPriceRules();
+                                $specificPrice = $this->product->getSpecificPrice();
+
+                                foreach($priceRules as $rule) {
+                                    ?>
+                                    <tr>
+                                        <td><?=$rule->getName()?></td>
+                                        <td><?=\CoreShop\Tool::formatPrice($rule->getPrice($this->product))?></td>
+                                        <td><?=\CoreShop\Tool::formatPrice($rule->getDiscount($specificPrice, $this->product))?></td>
+                                    </tr>
+                                    <?php
+                                }?>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <?php if($this->product->getTaxRule() instanceof \CoreShop\Model\TaxRuleGroup) { ?>
+                        <tr>
+                            <td><?=$this->translate("coreshop_taxrulegroups")?></td>
+                            <td><?=$this->product->getTaxRule()->getName()?> (<?=$this->product->getTaxRule()->getId()?>)</td>
+                        </tr>
                     <?php } ?>
                     <tr>
                         <td><?=$this->translate("coreshop_tax_rate")?></td>
                         <td><?=\CoreShop\Tool::formatTax($this->product->getTaxRate()/100)?></td>
                     </tr>
+
                 <?php } ?>
 
                 <?php if(\CoreShop\Tool::prepareCart()->getId()) { ?>
