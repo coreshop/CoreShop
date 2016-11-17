3715fb35d68d0004169db5c1f53e0ab24924b87e
dpfaffenbauer <dominik@lineofcode.at>
2016-11-17 17:27:34 +0100

    case #133: optimize update script

--- update/111/postupdate.php
+++ update/111/postupdate.php
@@ -2,4 +2,13 @@
 
 $db = \Pimcore\Db::get();
 
-$db->query("ALTER TABLE `coreshop_product_filters` ADD `similarities` text NOT NULL;");
+$data = $db->fetchAll("SHOW COLUMNS FROM coreshop_product_filters");
+$columns = [];
+
+foreach ($data as $d) {
+    $columns[] = $d["Field"];
+}
+
+if(!in_array('similarities', $columns)) {
+    $db->query("ALTER TABLE `coreshop_product_filters` ADD `similarities` text NOT NULL;");
+}
