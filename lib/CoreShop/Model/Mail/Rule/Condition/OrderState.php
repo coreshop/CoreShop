e78c2c349d6c2a6c34cbcbe6a225d9f2f53747c8
dpfaffenbauer <dominik@lineofcode.at>
2016-12-26 21:47:37 +0100

    case #148 WIP: implementing mail-rules OrderState Condition

---
+++ lib/CoreShop/Model/Mail/Rule/Condition/OrderState.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2016 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Model\Mail\Rule\Condition;
+
+use CoreShop\Model;
+use CoreShop\Model\Mail\Rule;
+use Pimcore\Model\AbstractModel;
+
+/**
+ * Class OrderState
+ * @package CoreShop\Model\Mail\Rule\Condition
+ */
+class OrderState extends AbstractCondition
+{
+    /**
+     *
+     */
+    const TRANSITION_TO = 1;
+
+    /**
+     *
+     */
+    const TRANSITION_FROM = 2;
+
+    /**
+     *
+     */
+    const TRANSITION_ALL = 3;
+
+    /**
+     * @var string
+     */
+    public $type = 'orderState';
+
+    /**
+     * @var array
+     */
+    public $states;
+
+    /**
+     * @var int
+     */
+    public $transitionType;
+
+    /**
+     * @param AbstractModel $object
+     * @param array $params
+     * @param Rule $rule
+     *
+     * @return boolean
+     */
+    public function checkCondition(AbstractModel $object, $params = [], Rule $rule)
+    {
+        if($object instanceof Model\Order) {
+            $paramsToExist = [
+                'fromState',
+                'toState'
+            ];
+
+            foreach($paramsToExist as $paramToExist) {
+                if(!array_key_exists($paramToExist, $params)) {
+                    return false;
+                }
+            }
+
+            $fromState = $params['fromState'];
+            $toState = $params['toState'];
+
+            if($this->getTransitionType() === self::TRANSITION_TO) {
+                if(in_array($toState, $this->getStates())) {
+                    return true;
+                }
+            }
+            else if($this->getTransitionType() === self::TRANSITION_FROM) {
+                if(in_array($fromState, $this->getStates())) {
+                    return true;
+                }
+            }
+            else if($this->getTransitionType() === self::TRANSITION_ALL) {
+                if(in_array($fromState, $this->getStates()) || in_array($toState, $this->getStates())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStates()
+    {
+        return $this->states;
+    }
+
+    /**
+     * @param array $states
+     */
+    public function setStates($states)
+    {
+        $this->states = $states;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTransitionType()
+    {
+        return $this->transitionType;
+    }
+
+    /**
+     * @param int $transitionType
+     */
+    public function setTransitionType($transitionType)
+    {
+        $this->transitionType = $transitionType;
+    }
+}
---
+++ static/js/coreshop/mail/rules/conditions/orderState.js
@@ -0,0 +1,44 @@
+/**
+ * CoreShop
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2016 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+pimcore.registerNS('pimcore.plugin.coreshop.mail.rules.conditions.orderState');
+
+pimcore.plugin.coreshop.mail.rules.conditions.orderState = Class.create(pimcore.plugin.coreshop.rules.conditions.abstract, {
+    type : 'orderState',
+
+    getForm : function () {
+        this.form = Ext.create('Ext.form.FieldSet', {
+            items : [
+                {
+                    xtype : 'label',
+                    html: 'IMPLEMENT ME'
+                },
+                {
+                    xtype: 'combo',
+                    fieldLabel: t('coreshop_transition_direction'),
+                    name: 'transitionType',
+                    value: this.data ? this.data.transitionType : 3,
+                    width: 250,
+                    store: [[1, t('coreshop_transition_to')], [2, t('coreshop_transition_from')], [3, t('coreshop_transition_all')]],
+                    triggerAction: 'all',
+                    typeAhead: false,
+                    editable: false,
+                    forceSelection: true,
+                    queryMode: 'local'
+                }
+            ]
+        });
+
+        return this.form;
+    }
+});
