543e11fd8c5eb521055afcb3a9b995f5f2cc7f83
dpfaffenbauer <dominik@lineofcode.at>
2016-12-27 12:16:16 +0100

    case #148 WIP: implementing mail-rules UserType Conditions

---
+++ lib/CoreShop/Model/Mail/Rule/Condition/User/UserType.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2016 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Model\Mail\Rule\Condition\User;
+
+use CoreShop\Model;
+use CoreShop\Model\Mail\Rule;
+use Pimcore\Model\AbstractModel;
+
+/**
+ * Class UserType
+ * @package CoreShop\Model\Mail\Rule\Condition\User
+ */
+class UserType extends Rule\Condition\AbstractCondition
+{
+    /**
+     * @var string
+     */
+    public $type = 'userType';
+
+    /**
+     *
+     */
+    const TYPE_NEW_ACCOUNT = 1;
+
+    /**
+     *
+     */
+    const TYPE_PASSWORD_RESET = 2;
+
+    /**
+     * @var int
+     */
+    public $userType;
+
+    /**
+     * @param AbstractModel $object
+     * @param array $params
+     * @param Rule $rule
+     *
+     * @return boolean
+     */
+    public function checkCondition(AbstractModel $object, $params = [], Rule $rule)
+    {
+        if($object instanceof Model\Order) {
+            $paramsToExist = [
+                'type'
+            ];
+
+            foreach($paramsToExist as $paramToExist) {
+                if(!array_key_exists($paramToExist, $params)) {
+                    return false;
+                }
+            }
+
+            $type = $params['type'];
+
+            if($this->getUserType() === $type) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserType()
+    {
+        return $this->userType;
+    }
+
+    /**
+     * @param int $userType
+     */
+    public function setUserType($userType)
+    {
+        $this->userType = $userType;
+    }
+}
