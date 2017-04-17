a2c727aa2904a6bd1a97f931d659d90ca09cfb63
dpfaffenbauer <dominik@lineofcode.at>
2017-04-17 20:22:38 +0200

    [CoreBundle] add Country Product Price Rule

---
+++ Bundle/CoreBundle/Product/ProductPriceRule/Condition/CountriesConditionChecker.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Product\ProductPriceRule\Condition;
+
+use CoreShop\Component\Address\Context\CountryContextInterface;
+use CoreShop\Component\Core\Model\CountryInterface;
+use CoreShop\Component\Product\Model\ProductInterface;
+use CoreShop\Component\Rule\Condition\ConditionCheckerInterface;
+use Webmozart\Assert\Assert;
+
+class CountriesConditionChecker implements ConditionCheckerInterface
+{
+    /**
+     * @var CountryContextInterface
+     */
+    private $countryContext;
+
+    /**
+     * @param CountryContextInterface $countryContext
+     */
+    public function __construct(CountryContextInterface $countryContext)
+    {
+        $this->countryContext = $countryContext;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid($subject, array $configuration)
+    {
+        Assert::isInstanceOf($subject, ProductInterface::class);
+
+        $country = $this->countryContext->getCountry();
+
+        if (!$country instanceof CountryInterface) {
+            return false;
+        }
+
+        return in_array($country->getId(), $configuration['countries']);
+    }
+}
