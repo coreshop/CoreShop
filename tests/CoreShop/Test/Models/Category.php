3b42f3a930b78057f009c99e8bfd43ac7c5650a4
dpfaffenbauer <dominik@lineofcode.at>
2017-04-27 11:18:45 +0200

    [UnitTest] add category test

--- tests/CoreShop/Test/Models/Category.php
+++ tests/CoreShop/Test/Models/Category.php
@@ -2,11 +2,25 @@
 
 namespace CoreShop\Test\Models;
 
+use CoreShop\Component\Product\Model\CategoryInterface;
 use CoreShop\Test\Base;
+use Pimcore\Model\Object\Service;
 
 class Category extends Base
 {
     public function testCategoryCreation() {
-        $this->printTodoTestName();
+        $this->printTestName();
+
+        /**
+         * @var $category CategoryInterface
+         */
+        $category = $this->getFactory('category')->createNew();
+        $category->setName('test');
+        $category->setPublished(true);
+        $category->setKey('test-category');
+        $category->setParent(Service::createFolderByPath('/'));
+        $category->save();
+
+        $this->assertNotNull($category->getid());
     }
 }
