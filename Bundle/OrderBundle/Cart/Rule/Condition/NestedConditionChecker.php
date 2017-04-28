8202b4253de4fe82df1d277ed268cbd8640462c7
dpfaffenbauer <dominik@lineofcode.at>
2017-04-28 13:25:18 +0200

    [OrderBundle] implement Nested Condition

--- Bundle/OrderBundle/Cart/Rule/Condition/NestedConditionChecker.php
+++ Bundle/OrderBundle/Cart/Rule/Condition/NestedConditionChecker.php
@@ -4,15 +4,45 @@ namespace CoreShop\Bundle\OrderBundle\Cart\Rule\Condition;
 
 use CoreShop\Component\Product\Model\ProductInterface;
 use CoreShop\Component\Rule\Condition\ConditionCheckerInterface;
+use CoreShop\Component\Rule\Condition\RuleConditionsValidationProcessorInterface;
 use Webmozart\Assert\Assert;
 
 class NestedConditionChecker implements ConditionCheckerInterface
 {
     /**
+     * @var RuleConditionsValidationProcessorInterface
+     */
+    protected $ruleConditionsValidationProcessor;
+
+    /**
+     * @param RuleConditionsValidationProcessorInterface $ruleConditionsValidationProcessor
+     */
+    public function __construct(RuleConditionsValidationProcessorInterface $ruleConditionsValidationProcessor)
+    {
+        $this->ruleConditionsValidationProcessor = $ruleConditionsValidationProcessor;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function isValid($subject, array $configuration)
     {
-        //TODO!!!
+        $operator = $configuration['operator'];
+
+        foreach ($configuration['conditions'] as $condition) {
+            $valid = $this->ruleConditionsValidationProcessor->isValid($subject, [$condition]);
+
+            if ($operator === "and") {
+                if (!$valid) {
+                    return false;
+                }
+            } elseif ($operator === "or") {
+                if ($valid) {
+                    return true;
+                }
+            }
+        }
+
+        return true;
     }
 }
