7ced90772a3b9070385261820bf22cba9ff929d0
dpfaffenbauer <dominik@lineofcode.at>
2017-05-02 17:26:39 +0100

    remove todo

--- Bundle/AdminBundle/Resources/public/js/coreshop/abstract/panel.js
+++ Bundle/AdminBundle/Resources/public/js/coreshop/abstract/panel.js
@@ -257,7 +257,6 @@ pimcore.registerNS('pimcore.plugin.coreshop.abstract.panel');
 
                         this.panels[panelKey] = new itemClass(this, res.data, panelKey, this.type, this.storeId);
                     } else {
-                        //TODO: Show messagebox
                         Ext.Msg.alert(t('open_target'), t('problem_opening_new_target'));
                     }
 
