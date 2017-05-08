a232d60321097deaf4af54ca57baed5650ec2de6
dpfaffenbauer <dominik@lineofcode.at>
2017-05-08 22:47:51 +0200

    [Tests] gitlab-ci

--- tests/run-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-$VENDOR_DIR/bin/phpunit -c $VENDOR_DIR/coreshop/core-shop/tests/config/phpunit.xml --bootstrap $VENDOR_DIR/coreshop/core-shop/tests/bootstrap.php $VENDOR_DIR/coreshop/core-shop/tests/AllTests
