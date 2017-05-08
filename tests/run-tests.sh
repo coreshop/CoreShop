0b383148df5d03e21938dce750dbd94d1c0fcc22
dpfaffenbauer <dominik@lineofcode.at>
2017-05-08 21:23:55 +0200

    [Tests] gitlab-ci

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd src/CoreShop
-
 set -e
 
 # switch interpreter
 if [[ "$TRAVIS_PHP_VERSION" == *"hhvm"* ]]; then CMD="hhvm"; else CMD="php"; fi
 
-CMD="$CMD -c config/phpunit.xml --bootstrap bootstrap.php  --log-json output/log.xml AllTests"
+CMD="$CMD phpunit.php -c config/phpunit.xml --bootstrap bootstrap.php  --log-json output/log.xml AllTests"
 
 echo $CMD
 eval $CMD
