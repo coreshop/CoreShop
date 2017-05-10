2da8774aeedbca58f23835e77d8e58f9e5e6e41a
dpfaffenbauer <dominik@lineofcode.at>
2017-05-10 18:00:11 +0200

    [OrderBundle] add Core\Product class which glues OrderComponent with ProductComponent

---
+++ Component/Core/Model/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CoreShop\Component\Core\Model;
+
+use CoreShop\Component\Order\Model\PurchasableInterface;
+use CoreShop\Component\Product\Model\Product as BaseProduct;
+
+class Product extends BaseProduct implements PurchasableInterface
+{
+
+}
