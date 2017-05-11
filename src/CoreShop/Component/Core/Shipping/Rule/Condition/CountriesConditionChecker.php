187c1ca5d566a4b264c07b7240539385529dab40
dpfaffenbauer <dominik@lineofcode.at>
2017-05-11 22:13:07 +0200

    [Shipping] fix Shipping Rule Country Condition

---
+++ src/CoreShop/Component/Core/Shipping/Rule/Condition/CountriesConditionChecker.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Component\Core\Shipping\Rule\Condition;
+
+use CoreShop\Component\Address\Model\AddressInterface;
+use CoreShop\Component\Shipping\Model\CarrierInterface;
+use CoreShop\Component\Product\Model\ProductInterface;
+use CoreShop\Component\Resource\Model\ResourceInterface;
+use CoreShop\Component\Shipping\Model\ShippableInterface;
+use CoreShop\Component\Shipping\Rule\Condition\AbstractConditionChecker;
+
+class CountriesConditionChecker extends AbstractConditionChecker
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isShippingRuleValid(CarrierInterface $carrier, ShippableInterface $shippable, AddressInterface $address, array $configuration)
+    {
+        $country = $address->getCountry();
+
+        return in_array($country->getId(), $configuration['countries']);
+    }
+}
