e226bf1e1ddc89deaee547cf83249708967a9220
dpfaffenbauer <dominik@lineofcode.at>
2017-06-19 15:48:53 +0200

    [CoreBundle] add command to install demo-data

---
+++ src/CoreShop/Bundle/CoreBundle/Command/InstallDemoDataCommand.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\CoreBundle\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class InstallDemoDataCommand extends AbstractInstallCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('coreshop:install:demo')
+            ->setDescription('Install CoreShop Demo Data.')
+            ->setHelp(<<<EOT
+The <info>%command.name%</info> command install CoreShop Demo Data.
+EOT
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->runCommands(['okvpn:migration:data:load' => ['--fixtures-type=demo']], $output);
+    }
+}
