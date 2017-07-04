0ff090e40246ae0db3b095c91d1e92fdd501ab7f
dpfaffenbauer <dominik@lineofcode.at>
2017-07-04 20:35:32 +0200

    [ResourceBundle] add PimcoreClassChoiceType

---
+++ src/CoreShop/Bundle/ResourceBundle/Form/Type/PimcoreClassChoiceType.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\ResourceBundle\Form\Type;
+
+use Pimcore\Model\Object\ClassDefinition;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class PimcoreClassChoiceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $classes = new ClassDefinition\Listing();
+        $classes = $classes->load();
+
+        $choices = [];
+
+        foreach ($classes as $class) {
+            $choices[$class->getName()] = $class->getName();
+        }
+
+        $resolver->setDefaults([
+            'choices' => $choices,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'coreshop_resource_pimcore_class_choice';
+    }
+}
