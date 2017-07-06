79fb4063bb73f84e360e42083751c3be1d8ececb
dpfaffenbauer <dominik@lineofcode.at>
2017-07-06 08:40:19 +0200

    [CoreBundle] fix coding style

--- src/CoreShop/Bundle/CoreBundle/Migrations/Data/Demo/ORM/CategoryFixture.php
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Data/Demo/ORM/CategoryFixture.php
@@ -8,7 +8,7 @@
  *
  * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
  * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
-*/
+ */
 
 namespace CoreShop\Bundle\CoreBundle\Migrations\Data\Demo\ORM;
 
@@ -53,7 +53,7 @@ class CategoryFixture extends AbstractFixture implements ContainerAwareInterface
             $categoriesCount = 5;
             $faker = Factory::create();
 
-            for($i = 0; $i < $categoriesCount; $i++) {
+            for ($i = 0; $i < $categoriesCount; $i++) {
                 /**
                  * @var $category CategoryInterface
                  */
