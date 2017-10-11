c5604dfffe81aab9781d1714e67c2f9d2722a46a
dpfaffenbauer <dominik@lineofcode.at>
2017-10-11 09:03:49 +0200

    [CoreBundle] simplify product class index helper

--- src/CoreShop/Bundle/CoreBundle/Index/ProductClassHelper.php
+++ src/CoreShop/Bundle/CoreBundle/Index/ProductClassHelper.php
@@ -42,25 +42,24 @@ class ProductClassHelper implements ClassHelperInterface
             $parentCategoryIds = [];
 
             $categories = $indexable->getCategories();
+            $categories = is_array($categories) ? $categories : [];
 
-            if (is_array($categories) && count($categories) > 0) {
-                foreach ($categories as $c) {
-                    if ($c instanceof CategoryInterface) {
-                        $categoryIds[$c->getId()] = $c->getId();
+            foreach ($categories as $c) {
+                if ($c instanceof CategoryInterface) {
+                    $categoryIds[$c->getId()] = $c->getId();
 
-                        $parents = $c->getHierarchy();
+                    $parents = $c->getHierarchy();
 
-                        foreach ($parents as $p) {
-                            $parentCategoryIds[] = $p->getId();
-                        }
+                    foreach ($parents as $p) {
+                        $parentCategoryIds[] = $p->getId();
                     }
                 }
             }
-
+            
             return [
                 'categoryIds' => implode(',', $categoryIds) . ',',
                 'parentCategoryIds' => ',' . implode(',', $parentCategoryIds) . ',',
-                'stores' => ','.@implode(',', $indexable->getStores()).','
+                'stores' => ',' . @implode(',', $indexable->getStores()) . ','
             ];
         }
 
