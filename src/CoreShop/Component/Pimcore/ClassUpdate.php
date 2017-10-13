ad22c0eb5051e776752c8e60116c7e2e386ab716
dpfaffenbauer <dominik@lineofcode.at>
2017-10-13 11:07:36 +0200

    [Pimcore] add ClassUpdate Helper -> supports "insertFieldBefore", "insertFieldAfter", "replaceField", "replaceFieldProperties" and "removeField" of pimcore classes

---
+++ src/CoreShop/Component/Pimcore/ClassUpdate.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Component\Pimcore;
+
+use Pimcore\Model\DataObject;
+
+class ClassUpdate
+{
+    /**
+     * @var string
+     */
+    private $className;
+
+    /**
+     * @var DataObject\ClassDefinition
+     */
+    private $classDefinition;
+
+    /**
+     * @var array
+     */
+    private $jsonDefinition;
+
+    /**
+     * @var array
+     */
+    private $classFieldDefinitions;
+
+    /**
+     * @param $className
+     * @throws ClassDefinitionNotFoundException
+     */
+    public function __construct($className)
+    {
+        $this->className = $className;
+        $this->classDefinition = DataObject\ClassDefinition::getByName($className);
+
+        if (is_null($this->classDefinition)) {
+            throw new ClassDefinitionNotFoundException(sprintf('ClassDefinition %s not found', $className));
+        }
+
+        $this->classFieldDefinitions = $this->classDefinition->getFieldDefinitions();
+        $this->jsonDefinition = json_decode(DataObject\ClassDefinition\Service::generateClassDefinitionJson($this->classDefinition), true);
+    }
+
+    /**
+     * Save Field Definition
+     *
+     * @return bool
+     */
+    public function save()
+    {
+        return DataObject\ClassDefinition\Service::importClassDefinitionFromJson($this->classDefinition, json_encode($this->jsonDefinition), true);
+    }
+
+    /**
+     * Check if Class has field
+     *
+     * @param $fieldName
+     * @return bool
+     */
+    public function hasField($fieldName)
+    {
+        return array_key_exists($fieldName, $this->classFieldDefinitions);
+    }
+
+    /**
+     * Insert Field before another field
+     *
+     * @param $fieldName
+     * @param $jsonFieldDefinition
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    public function insertFieldBefore($fieldName, $jsonFieldDefinition)
+    {
+        $this->findField($fieldName, function (&$foundField, $index, &$parent) use ($jsonFieldDefinition) {
+            if ($index === 0) {
+                $index = 1;
+            }
+
+            $childs = $parent['childs'];
+
+            array_splice($childs, $index, 0, [$jsonFieldDefinition]);
+
+            $parent['childs'] = $childs;
+        });
+    }
+
+    /**
+     * Insert Field after another field
+     *
+     * @param $fieldName
+     * @param $jsonFieldDefinition
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    public function insertFieldAfter($fieldName, $jsonFieldDefinition)
+    {
+        $this->findField($fieldName, function (&$foundField, $index, &$parent) use ($jsonFieldDefinition) {
+            $childs = $parent['childs'];
+
+            array_splice($childs, $index + 1, 0, [$jsonFieldDefinition]);
+
+            $parent['childs'] = $childs;
+        });
+    }
+
+    /**
+     * Replace existing Field with a new Definition
+     *
+     * @param $fieldName
+     * @param $jsonFieldDefinition
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    public function replaceField($fieldName, $jsonFieldDefinition)
+    {
+        $this->findField($fieldName, function (&$foundField, $index, &$parent) use ($jsonFieldDefinition) {
+            $foundField = $jsonFieldDefinition;
+        });
+    }
+
+    /**
+     * Replace Properties from any field
+     *
+     * @param $fieldName
+     * @param array $keyValues
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    public function replaceFieldProperties($fieldName, array $keyValues)
+    {
+        $this->findField($fieldName, function (&$foundField, $index, &$parent) use ($keyValues) {
+            foreach ($keyValues as $key => $value) {
+                $foundField[$key] = $value;
+            }
+        });
+    }
+
+    /**
+     * Remove existing Field
+     *
+     * @param $fieldName
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    public function removeField($fieldName)
+    {
+        $this->findField($fieldName, function (&$foundField, $index, &$parent) {
+            unset($parent['childs'][$index]);
+        });
+    }
+
+    /**
+     * @param string $fieldName
+     * @param \Closure $callback
+     *
+     * @throws ClassDefinitionFieldNotFoundException
+     */
+    protected function findField(string $fieldName, \Closure $callback)
+    {
+        $found = false;
+
+        $traverseFunction = function ($children) use (&$traverseFunction, $fieldName, $callback, &$found) {
+            foreach ($children['childs'] as $index => &$child) {
+                if ($child['name'] === $fieldName) {
+                    $callback($child, $index, $children);
+                    $found = true;
+                    break;
+                } else {
+                    if (array_key_exists('childs', $child)) {
+                        $child = $traverseFunction($child);
+                    }
+                }
+            }
+
+            return $children;
+        };
+
+        $this->jsonDefinition['layoutDefinitions'] = $traverseFunction($this->jsonDefinition['layoutDefinitions']);
+
+        if (!$found) {
+            throw new ClassDefinitionFieldNotFoundException(sprintf('Field with name %s not found', $fieldName));
+        }
+    }
+}
