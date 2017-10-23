81725a091a5cf96e90d26432bfb835d84a49f774
dpfaffenbauer <dominik@lineofcode.at>
2017-10-23 20:52:01 +0200

    [CoreBundle] remove abstract functions from trait, this could cause problems when updating pimcore

--- src/CoreShop/Bundle/CoreBundle/Controller/CoreSaleCreationTrait.php
+++ src/CoreShop/Bundle/CoreBundle/Controller/CoreSaleCreationTrait.php
@@ -143,25 +143,4 @@ trait CoreSaleCreationTrait
 
         return $cart;
     }
-
-    /**
-     * Gets a container service by its id.
-     *
-     * @param string $id The service id
-     *
-     * @return object The service
-     */
-    protected abstract function get($id);
-
-    /**
-     * Returns a JsonResponse that uses the serializer component if enabled, or json_encode.
-     *
-     * @param mixed $data    The response data
-     * @param int   $status  The status code to use for the Response
-     * @param array $headers Array of extra headers to add
-     * @param array $context Context to pass to serializer when using serializer component
-     *
-     * @return JsonResponse
-     */
-    protected abstract function json($data, $status = 200, $headers = [], $context = []);
 }
