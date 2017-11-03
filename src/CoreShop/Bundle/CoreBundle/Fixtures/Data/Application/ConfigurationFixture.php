721fa854f52e43c55d77a94242fd44243258cc9c
dpfaffenbauer <dominik@lineofcode.at>
2017-11-03 17:35:26 +0100

    [CoreBundle] remove settings that don't exist anymore

--- src/CoreShop/Bundle/CoreBundle/Fixtures/Data/Application/ConfigurationFixture.php
+++ src/CoreShop/Bundle/CoreBundle/Fixtures/Data/Application/ConfigurationFixture.php
@@ -47,30 +47,13 @@ class ConfigurationFixture extends AbstractFixture implements ContainerAwareInte
     public function load(ObjectManager $manager)
     {
         $configurations = [
-            'system.catalog.mode' => false,
             'system.guest.checkout' => true,
-            'system.stock.default_out_of_stock.behaviour' => 1,
-            'system.taxation.address' => 'shipping',
-            'system.shipment.create' => true,
-            'system.shipment.prefix' => 'SH',
-            'system.shipment.suffix' => '',
-            'system.shipment.wkhtml' => '-T 40mm -B 15mm -L 10mm -R 10mm --header-spacing 5 --footer-spacing 5',
-            'system.product.fallback_image' => null,
-            'system.category.fallback_image' => null,
             'system.category.list.mode' => 'list',
             'system.category.list.per_page' => [12, 24, 36],
             'system.category.list.per_page.default' => 12,
             'system.category.grid.per_page' => [5, 10, 15, 20, 25],
             'system.category.grid.per_page.default' => 10,
             'system.category.variant_mode' => 'hide',
-            'system.order.prefix' => 'O',
-            'system.order.suffix' => '',
-            'system.quote.prefix' => 'Q',
-            'system.quote.suffix' => '',
-            'system.invoice.create' => true,
-            'system.invoice.prefix' => 'IN',
-            'system.invoice.suffix' => '',
-            'system.invoice.wkhtml' => '-T 40mm -B 15mm -L 10mm -R 10mm --header-spacing 5 --footer-spacing 5',
         ];
 
         foreach ($configurations as $key => $value) {
