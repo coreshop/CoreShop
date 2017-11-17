f0503eb375eb34288ff647fe9a308d18e707ee64
dpfaffenbauer <dominik@lineofcode.at>
2017-11-17 16:06:36 +0000

    [IndexBundle, Index] adding QuantityValue Interpreter

---
+++ src/CoreShop/Component/Index/Interpreter/QuantityValueInterpreter.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Component\Index\Interpreter;
+
+use CoreShop\Component\Index\Model\IndexColumnInterface;
+use Pimcore\Model\DataObject\AbstractObject;
+use Pimcore\Model\DataObject\Data\QuantityValue;
+
+class QuantityValueInterpreter implements InterpreterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function interpret($value, IndexColumnInterface $config = null)
+    {
+        if ($value instanceof QuantityValue) {
+            return $value->getValue();
+        }
+
+        return null;
+    }
+}
