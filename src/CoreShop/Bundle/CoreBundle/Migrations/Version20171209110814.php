0ec5d54f6210c350795e3402cd6c8171a2ceb706
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-12-09 12:16:44 +0100

    [CoreBundle, OrderBundle] create additionalData migration. see #222

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20171209110814.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use CoreShop\Component\Pimcore\ClassUpdate;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Version20171209110814 extends AbstractPimcoreMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     * @throws \CoreShop\Component\Pimcore\ClassDefinitionFieldNotFoundException
+     */
+    public function up(Schema $schema)
+    {
+        $additionalDataField = [
+            'fieldtype'       => 'objectbricks',
+            'phpdocType'      => '\\Pimcore\\Model\\DataObject\\Objectbrick',
+            'allowedTypes'    => [],
+            'maxItems'        => '',
+            'name'            => 'additionalData',
+            'title'           => 'Additional Data',
+            'tooltip'         => '',
+            'mandatory'       => FALSE,
+            'noteditable'     => FALSE,
+            'index'           => FALSE,
+            'locked'          => NULL,
+            'style'           => '',
+            'permissions'     => NULL,
+            'datatype'        => 'data',
+            'columnType'      => NULL,
+            'queryColumnType' => NULL,
+            'relationType'    => FALSE,
+            'invisible'       => FALSE,
+            'visibleGridView' => FALSE,
+            'visibleSearch'   => FALSE,
+        ];
+
+        $cartClass = $this->container->getParameter('coreshop.model.cart.pimcore_class_name');
+        $classUpdater = new ClassUpdate($cartClass);
+        if (!$classUpdater->hasField('additionalData')) {
+            $classUpdater->insertFieldAfter('comment', $additionalDataField);
+            $classUpdater->save();
+        }
+
+        $orderClass = $this->container->getParameter('coreshop.model.order.pimcore_class_name');
+        $classUpdater = new ClassUpdate($orderClass);
+        if (!$classUpdater->hasField('additionalData')) {
+            $classUpdater->insertFieldAfter('comment', $additionalDataField);
+            $classUpdater->save();
+        }
+
+        $quoteClass = $this->container->getParameter('coreshop.model.quote.pimcore_class_name');
+        $classUpdater = new ClassUpdate($quoteClass);
+        if (!$classUpdater->hasField('additionalData')) {
+            $classUpdater->insertFieldAfter('comment', $additionalDataField);
+            $classUpdater->save();
+        }
+
+        //update translations
+        $this->container->get('coreshop.resource.installer.shared_translations')->installResources(new NullOutput(), 'coreshop');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
