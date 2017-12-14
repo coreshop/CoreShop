92cff698b09573bd8f9ae3384cd7e33431db31e9
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-12-14 18:29:16 +0100

    [ResourceBundle] restrict searchEditor for given class in objectMultiHref

--- src/CoreShop/Bundle/ResourceBundle/Resources/public/pimcore/js/object/objectMultihref.js
+++ src/CoreShop/Bundle/ResourceBundle/Resources/public/pimcore/js/object/objectMultihref.js
@@ -215,7 +215,7 @@ coreshop.object.objectMultihref = Class.create(pimcore.object.tags.objects, {
         if (this.fieldConfig.classes != null && this.fieldConfig.classes.length > 0) {
             allowedClasses = [];
             for (var i = 0; i < this.fieldConfig.classes.length; i++) {
-                allowedClasses.push(this.fieldConfig.classes[i]);
+                allowedClasses.push(this.fieldConfig.classes[i].classes);
             }
         }
 
