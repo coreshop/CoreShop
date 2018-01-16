6c4a07c5e1b5cb4ba83e6bc769d551e8cbabec80
Stefan Hagspiel <shagspiel@dachcom.ch>
2018-01-16 19:01:58 +0100

    [Core] store PaymentSettings in Order

--- src/CoreShop/Component/Core/Order/Transformer/CartToSaleTransformer.php
+++ src/CoreShop/Component/Core/Order/Transformer/CartToSaleTransformer.php
@@ -63,6 +63,7 @@ final class CartToSaleTransformer implements ProposalTransformerInterface
             if ($cart->getCarrier() instanceof CarrierInterface) {
                 $sale->setCarrier($cart->getCarrier());
                 $sale->setComment($cart->getComment());
+                $sale->setPaymentSettings($cart->getPaymentSettings());
                 $sale->setAdditionalData($cart->getAdditionalData());
                 $sale->setShipping($this->currencyConverter->convert($cart->getShipping(true), $fromCurrency, $toCurrency), true);
                 $sale->setShipping($this->currencyConverter->convert($cart->getShipping(false), $fromCurrency, $toCurrency), false);
