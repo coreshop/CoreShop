9881463b6d645336555353d59d0ebd84c640367c
dpfaffenbauer <dominik@lineofcode.at>
2018-01-23 20:58:31 +0100

    [CoreBundle] add product tax helper as PHP helper and use that for the twig helper

---
+++ src/CoreShop/Bundle/CoreBundle/Templating/Helper/ProductTaxHelper.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\CoreBundle\Templating\Helper;
+
+use CoreShop\Component\Core\Product\ProductTaxCalculatorFactoryInterface;
+use CoreShop\Component\Core\Product\TaxedProductPriceCalculatorInterface;
+use CoreShop\Component\Order\Model\PurchasableInterface;
+use CoreShop\Component\Taxation\Calculator\TaxCalculatorInterface;
+use Symfony\Component\Templating\Helper\Helper;
+
+class ProductTaxHelper extends Helper implements ProductTaxHelperInterface
+{
+    /**
+     * @var TaxedProductPriceCalculatorInterface
+     */
+    private $priceCalculator;
+
+    /**
+     * @var ProductTaxCalculatorFactoryInterface
+     */
+    private $taxCalculatorFactory;
+
+    /**
+     * @param TaxedProductPriceCalculatorInterface  $priceCalculator
+     * @param ProductTaxCalculatorFactoryInterface $taxCalculatorFactory
+     */
+    public function __construct(
+        TaxedProductPriceCalculatorInterface $priceCalculator,
+        ProductTaxCalculatorFactoryInterface $taxCalculatorFactory
+    ) {
+        $this->priceCalculator = $priceCalculator;
+        $this->taxCalculatorFactory = $taxCalculatorFactory;
+    }
+
+    /**
+     * @param PurchasableInterface $product
+     * @return array|int
+     */
+    public function getTaxAmount(PurchasableInterface $product)
+    {
+        $taxCalculator = $this->taxCalculatorFactory->getTaxCalculator($product);
+        if ($taxCalculator instanceof TaxCalculatorInterface) {
+            return $taxCalculator->getTaxesAmount($this->priceCalculator->getPrice($product, false));
+        }
+    }
+
+    /**
+     * @param PurchasableInterface $product
+     * @return float
+     */
+    public function getTaxRate(PurchasableInterface $product)
+    {
+        $taxCalculator = $this->taxCalculatorFactory->getTaxCalculator($product);
+        if ($taxCalculator instanceof TaxCalculatorInterface) {
+            return $taxCalculator->getTotalRate();
+        }
+    }
+
+    public function getName()
+    {
+        return 'coreshop_product_tax';
+    }
+}
--- src/CoreShop/Bundle/CoreBundle/Twig/ProductTaxExtension.php
+++ src/CoreShop/Bundle/CoreBundle/Twig/ProductTaxExtension.php
@@ -12,35 +12,21 @@
 
 namespace CoreShop\Bundle\CoreBundle\Twig;
 
-use CoreShop\Component\Core\Product\ProductTaxCalculatorFactoryInterface;
-use CoreShop\Component\Core\Product\TaxedProductPriceCalculatorInterface;
-use CoreShop\Component\Core\Taxation\TaxApplicatorInterface;
-use CoreShop\Component\Order\Calculator\PurchasablePriceCalculatorInterface;
-use CoreShop\Component\Order\Model\PurchasableInterface;
-use CoreShop\Component\Taxation\Calculator\TaxCalculatorInterface;
+use CoreShop\Bundle\CoreBundle\Templating\Helper\ProductTaxHelperInterface;
 
 final class ProductTaxExtension extends \Twig_Extension
 {
     /**
-     * @var TaxedProductPriceCalculatorInterface
+     * @var ProductTaxHelperInterface
      */
-    private $priceCalculator;
+    private $productTaxHelper;
 
     /**
-     * @var ProductTaxCalculatorFactoryInterface
+     * @param ProductTaxHelperInterface $productTaxHelper
      */
-    private $taxCalculatorFactory;
-
-    /**
-     * @param TaxedProductPriceCalculatorInterface  $priceCalculator
-     * @param ProductTaxCalculatorFactoryInterface $taxCalculatorFactory
-     */
-    public function __construct(
-        TaxedProductPriceCalculatorInterface $priceCalculator,
-        ProductTaxCalculatorFactoryInterface $taxCalculatorFactory
-    ) {
-        $this->priceCalculator = $priceCalculator;
-        $this->taxCalculatorFactory = $taxCalculatorFactory;
+    public function __construct(ProductTaxHelperInterface $productTaxHelper)
+    {
+        $this->productTaxHelper = $productTaxHelper;
     }
 
     /**
@@ -49,32 +35,8 @@ final class ProductTaxExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_Filter('coreshop_product_tax_amount', [$this, 'getTaxAmount']),
-            new \Twig_Filter('coreshop_product_tax_rate', [$this, 'getTaxRate']),
+            new \Twig_Filter('coreshop_product_tax_amount', [$this->productTaxHelper, 'getTaxAmount']),
+            new \Twig_Filter('coreshop_product_tax_rate', [$this->productTaxHelper, 'getTaxRate']),
         ];
-    }
-
-    /**
-     * @param PurchasableInterface $product
-     * @return array|int
-     */
-    public function getTaxAmount(PurchasableInterface $product)
-    {
-        $taxCalculator = $this->taxCalculatorFactory->getTaxCalculator($product);
-        if ($taxCalculator instanceof TaxCalculatorInterface) {
-            return $taxCalculator->getTaxesAmount($this->priceCalculator->getPrice($product, false));
-        }
-    }
-
-    /**
-     * @param PurchasableInterface $product
-     * @return float
-     */
-    public function getTaxRate(PurchasableInterface $product)
-    {
-        $taxCalculator = $this->taxCalculatorFactory->getTaxCalculator($product);
-        if ($taxCalculator instanceof TaxCalculatorInterface) {
-            return $taxCalculator->getTotalRate();
-        }
     }
 }
