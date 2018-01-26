fa7ea20943426d2e3f07febfc11fea680284687f
dpfaffenbauer <dominik@lineofcode.at>
2018-01-26 09:36:18 +0100

    [ResourceBundle] add grid-config installer

---
+++ src/CoreShop/Bundle/ResourceBundle/Installer/Configuration/GridConfigConfiguration.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\ResourceBundle\Installer\Configuration;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class GridConfigConfiguration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('grid_config');
+
+        $rootNode
+            ->children()
+                ->arrayNode('grid_config')
+                    ->useAttributeAsKey('key')
+                    ->arrayPrototype()
+                        ->addDefaultsIfNotSet()
+                        ->children()
+                            ->scalarNode('key')->cannotBeEmpty()->end()
+                            ->scalarNode('name')->cannotBeEmpty()->end()
+                            ->scalarNode('class')->cannotBeEmpty()->end()
+                            ->variableNode('data')->cannotBeEmpty()->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ src/CoreShop/Bundle/ResourceBundle/Installer/PimcoreGridConfigInstaller.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\ResourceBundle\Installer;
+
+use CoreShop\Bundle\ResourceBundle\Installer\Configuration\GridConfigConfiguration;
+use CoreShop\Component\Pimcore\GridConfigInstallerInterface;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Yaml\Yaml;
+
+final class PimcoreGridConfigInstaller implements ResourceInstallerInterface
+{
+    /**
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+    /**
+     * @var array
+     */
+    protected $classIds;
+
+    /**
+     * @var GridConfigInstallerInterface
+     */
+    protected $gridConfigInstaller;
+
+    /**
+     * @param KernelInterface $kernel
+     * @param array $classIds
+     * @param GridConfigInstallerInterface $gridConfigInstaller
+     */
+    public function __construct(
+        KernelInterface $kernel,
+        array $classIds,
+        GridConfigInstallerInterface $gridConfigInstaller
+    )
+    {
+        $this->kernel = $kernel;
+        $this->classIds = $classIds;
+        $this->gridConfigInstaller = $gridConfigInstaller;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function installResources(OutputInterface $output, $applicationName = null)
+    {
+        $parameter = $applicationName ? sprintf('%s.application.pimcore.admin.install.grid_config', $applicationName) : 'resources.admin.install.grid_config';
+
+        if ($this->kernel->getContainer()->hasParameter($parameter)) {
+            $routeFilesToInstall = $this->kernel->getContainer()->getParameter($parameter);
+            $gridConfigsToInstall = [];
+
+            $progress = new ProgressBar($output);
+            $progress->setBarCharacter('<info>░</info>');
+            $progress->setEmptyBarCharacter(' ');
+            $progress->setProgressCharacter('<comment>░</comment>');
+            $progress->setFormat(' %current%/%max% [%bar%] %percent:3s%% %message%');
+
+            $processor = new Processor();
+            $configurationDefinition = new GridConfigConfiguration();
+
+            foreach ($routeFilesToInstall as $file) {
+                $file = $this->kernel->locateResource($file);
+
+                if (file_exists($file)) {
+                    $gridConfigs = Yaml::parse(file_get_contents($file));
+                    $gridConfigs = $processor->processConfiguration($configurationDefinition, ['grid_config' => $gridConfigs]);
+                    $gridConfigs = $gridConfigs['grid_config'];
+
+                    foreach ($gridConfigs as $name => $gridConfigData) {
+                        $gridConfigsToInstall[$name] = $gridConfigData;
+                    }
+                }
+            }
+
+            $progress->start(count($gridConfigsToInstall));
+
+            foreach ($gridConfigsToInstall as $name => $gridData) {
+                $progress->setMessage(sprintf('<error>Install Grid Config %s</error>', $name));
+
+                $this->gridConfigInstaller->installGridConfig($gridData['data'], $gridData['name'], $this->classIds[$gridData['class']], true);
+
+                $progress->advance();
+            }
+
+            $progress->finish();
+        }
+    }
+}
