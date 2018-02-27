51a1d19c26d472b7d589b3f27675606617067bea
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-02-27 20:44:04 +0100

    [Tests] remove codeception autoloader

--- tests/behat-bootstrap.php
+++ tests/behat-bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-use Pimcore\Tests\Util\Autoloader;
-
 if (!defined('PIMCORE_PROJECT_ROOT')) {
     define(
         'PIMCORE_PROJECT_ROOT',
@@ -19,12 +17,10 @@ define('PIMCORE_CLASS_DIRECTORY', __DIR__ . '/tmp/var/classes');
 
 define('PIMCORE_TEST', true);
 
-require_once PIMCORE_PROJECT_ROOT . '/pimcore/tests/_support/Util/Autoloader.php';
 require_once PIMCORE_PROJECT_ROOT . '/pimcore/config/bootstrap.php';
 
 /**
  * @var $loader \Composer\Autoload\ClassLoader
  */
 $loader->add('CoreShop\Test', [__DIR__.'/lib']);
-
-Autoloader::addNamespace('Pimcore\Model\DataObject', __DIR__ . '/tmp/var/classes/DataObject');
+$loader->add('Pimcore\Model\DataObject', [__DIR__ . '/tmp/var/classes/DataObject']);
