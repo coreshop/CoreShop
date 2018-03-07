c972098bb158eb7ff6bc4f64e41b75400d39cfe6
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-03-07 17:13:55 +0100

    [OrderDetail] refactor order-detail page and split it up into several blocks, each block then is responisble for displaying data and updating data on refresh

--- src/CoreShop/Bundle/OrderBundle/Resources/public/pimcore/js/sale/create/eventManager.js
+++
@@ -1,7 +0,0 @@
-Ext.define('CoreShop.order.sale.create.EventManager', {
-    mixins: ['Ext.mixin.Observable'],
-
-    constructor: function () {
-        this.mixins.observable.constructor.call(this, {});
-    }
-});
