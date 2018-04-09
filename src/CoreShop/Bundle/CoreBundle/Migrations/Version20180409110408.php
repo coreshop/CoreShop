693f7a8cf8dfc05bf85fd380143da51f0e37a987
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-04-09 13:05:47 +0200

    [CoreBundle] add migration to drop rangeBehaviour column fixes #401

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20180409110408.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+
+class Version20180409110408 extends AbstractPimcoreMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        if ($schema->getTable('coreshop_carrier')->hasColumn('rangeBehaviour')) {
+            $this->addSql('ALTER TABLE coreshop_carrier DROP rangeBehaviour;');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+
+    }
+}
