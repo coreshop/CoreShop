2c49df98b053d22f28aa29f15a181c3b02ba4943
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-04-10 09:02:03 +0200

    [CoreBundle] add migration for cart-locale

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20180410063351.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use CoreShop\Component\Core\Model\OrderInterface;
+use CoreShop\Component\Customer\Model\CustomerInterface;
+use CoreShop\Component\Order\Model\CartInterface;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Pimcore\Tool;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Version20180410063351 extends AbstractPimcoreMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $list = $this->container->get('coreshop.repository.cart')->getList();
+        $list->setCondition('localeCode IS NULL');
+        $list->load();
+
+        /**
+         * @var $cart CartInterface
+         */
+        foreach ($list->getObjects() as $cart) {
+            $order = $cart->getOrder();
+            $customer = $cart->getCustomer();
+
+            if ($order instanceof OrderInterface) {
+                $cart->setLocaleCode($order->getOrderLanguage());
+            }
+            else if ($customer instanceof CustomerInterface && null !== $customer->getLocale()) {
+                $cart->setLocaleCode($customer->getLocale());
+            } else {
+                $cart->setLocaleCode(Tool::getDefaultLanguage());
+            }
+
+            $cart->save();
+        }
+    }e
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
