d4ef57f8583bd4fde3897c543d8551e68146353b
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-05-13 18:55:31 +0200

    [PimcoreBundle] introduce a new PimcoreBundle which comes with a lot of handy extensions for Pimcore

---
+++ src/CoreShop/Component/Pimcore/Twig/Extension/ObjectLinkGeneratorExtension.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Component\Pimcore\Twig\Extension;
+
+use Pimcore\Model\DataObject\Concrete;
+
+final class ObjectLinkGeneratorExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return [
+            new \Twig_Function('object_link', function (Concrete $object, array $params = []) {
+                if ($linkGenerator = $object->getClass()->getLinkGenerator()) {
+                    return $linkGenerator->generate(
+                        $object,
+                        $params
+                    );
+                }
+
+                throw new \InvalidArgumentException(sprintf('Object %s with class %s has no Link Generator configured', $object->getId(), $object->getClassName()));
+            }),
+        ];
+    }
+}
