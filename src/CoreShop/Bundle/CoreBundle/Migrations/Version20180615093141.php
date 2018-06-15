c34ffaa3d62e0f120103b214cb365a51288531e5
Martin Silber <info@martin-silber.de>
2018-06-15 12:33:16 +0200

    SilberMa: Change active, digitalProduct, isTracked from checkbox to booleanselect to solve variant and inheritance issues

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20180615093141.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use CoreShop\Component\Pimcore\DataObject\ClassUpdate;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Version20180615093141 extends AbstractPimcoreMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    private const CORE_SHOP_PRODUCT_FIELDS = [
+        [
+            'fieldName' => 'active',
+            'yesValue'  => 'active',
+            'noValue'   => 'inactive',
+        ],
+        [
+            'fieldName' => 'digitalProduct',
+            'yesValue'  => 'isDigitalProduct',
+            'noValue'   => 'noDigitalProduct',
+        ],
+        [
+            'fieldName' => 'isTracked',
+            'yesValue'  => 'isTracked',
+            'noValue'   => 'notTracked',
+        ],
+    ];
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $coreShopProductClass = $this->container->getParameter('coreshop.model.product.pimcore_class_name');
+
+        $classUpdater = new ClassUpdate($coreShopProductClass);
+
+        foreach (self::CORE_SHOP_PRODUCT_FIELDS as $updateField) {
+            if ($classUpdater->hasField($updateField['fieldName'])) {
+                $classUpdater->replaceFieldProperties(
+                    $updateField['fieldName'],
+                    [
+                        'fieldtype' => 'booleanSelect',
+                        'yesLabel'  => $updateField['yesValue'],
+                        'noLabel'   => $updateField['noValue'],
+                        'options'   => [
+                            0 => [
+                                'key'   => '',
+                                'value' => 0,
+                            ],
+                            1 => [
+                                'key'   => $updateField['yesValue'],
+                                'value' => 1,
+                            ],
+                            2 => [
+                                'key'   => $updateField['noValue'],
+                                'value' => -1,
+                            ],
+                        ],
+                    ]
+                );
+            }
+        }
+        $classUpdater->save();
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $coreShopProductClass = $this->container->getParameter('coreshop.model.product.pimcore_class_name');
+
+        $classUpdater = new ClassUpdate($coreShopProductClass);
+
+        foreach (self::CORE_SHOP_PRODUCT_FIELDS as $updateField) {
+            if ($classUpdater->hasField($updateField['fieldName'])) {
+                $classUpdater->replaceFieldProperties($updateField['fieldName'], ['fieldtype' => 'checkbox']);
+            }
+        }
+        $classUpdater->save();
+    }
+}
