599053c28a78e557f06698b3638f246acc73f7c9
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-06-16 11:01:04 +0200

    [Notification] implement store order mail type

---
+++ src/CoreShop/Bundle/CoreBundle/Form/Type/Notification/Action/StoreOrderMailConfigurationType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\Form\Type\Notification\Action;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class StoreOrderMailConfigurationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('mails', CollectionType::class, [
+                'allow_add' => true,
+                'allow_delete' => true,
+                'entry_type' => CollectionType::class,
+                'entry_options' => [
+                    'allow_add' => true,
+                    'allow_delete' => true,
+                ]
+            ])
+            ->add('sendInvoices', CheckboxType::class)
+            ->add('sendShipments', CheckboxType::class);;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'coreshop_notification_rule_action_store_mail';
+    }
+}
