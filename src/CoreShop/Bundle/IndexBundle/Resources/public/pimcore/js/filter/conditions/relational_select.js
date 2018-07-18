606ef2814772e5c7b9ab4f90b73c1c4e4ce5b625
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-07-18 14:45:37 +0200

    [Index] implement relational select

---
+++ src/CoreShop/Bundle/IndexBundle/Resources/public/pimcore/js/filter/conditions/relational_select.js
@@ -0,0 +1,38 @@
+/*
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ *
+ */
+
+pimcore.registerNS('coreshop.filter.conditions.relational_select');
+
+coreshop.filter.conditions.relational_select = Class.create(coreshop.filter.conditions.abstract, {
+    type: 'relational_select',
+
+    getItems: function () {
+        return [
+            this.getFieldsComboBox(),
+            {
+                xtype: 'combo',
+                fieldLabel: t('coreshop_filters_value'),
+                name: 'preSelects',
+                width: 400,
+                store: this.valueStore,
+                displayField: 'value',
+                valueField: 'key',
+                triggerAction: 'all',
+                typeAhead: false,
+                editable: false,
+                forceSelection: true,
+                queryMode: 'local',
+                value: this.data.configuration.preSelects
+            }
+        ];
+    }
+});
