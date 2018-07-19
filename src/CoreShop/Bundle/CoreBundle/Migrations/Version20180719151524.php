5bc69e7274c958aa6b29509a6e1e4e0de6d6bf8b
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-07-19 17:16:33 +0200

    [CoreBundle] rebuild classes migration

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20180719151524.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Pimcore\Model\DataObject\ClassDefinition;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Version20180719151524 extends AbstractPimcoreMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $classes = new ClassDefinition\Listing();
+        $classes = $classes->load();
+
+        foreach ($classes as $class) {
+            $class->save();
+        }
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
