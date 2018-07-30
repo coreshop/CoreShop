88265b63bebb8648c6daacaed5d5b9dc4727947d
Michal Bolka <mbolka@divante.pl>
2018-07-30 14:39:04 +0200

    Fix issue with calling delete on array

--- src/CoreShop/Bundle/ResourceBundle/Pimcore/ObjectManager.php
+++ src/CoreShop/Bundle/ResourceBundle/Pimcore/ObjectManager.php
@@ -137,8 +137,14 @@ final class ObjectManager implements \Doctrine\Common\Persistence\ObjectManager
      */
     public function flush()
     {
-        foreach ($this->modelsToRemove as $model) {
-            $model->delete();
+        foreach ($this->modelsToRemove as $models) {
+            if (is_array($models)) {
+                foreach ($models as $model) {
+                    $model->delete();
+                }
+            } else {
+                $models->delete();
+            }
         }
 
         foreach ([$this->modelsToInsert, $this->modelsToUpdate] as $modelsToSave) {
