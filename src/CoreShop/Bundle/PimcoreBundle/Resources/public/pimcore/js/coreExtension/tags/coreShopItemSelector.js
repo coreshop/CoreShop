f7dd105a24ff2b334b4bb36e0cd3d50d3d9d3a9f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-08-06 10:02:14 +0200

    [PimcoreBundle] add BC layer to Pimcore 5.4.0 and load correct directory for ext.ux files

--- src/CoreShop/Bundle/PimcoreBundle/Resources/public/pimcore/js/coreExtension/tags/coreShopItemSelector.js
+++ src/CoreShop/Bundle/PimcoreBundle/Resources/public/pimcore/js/coreExtension/tags/coreShopItemSelector.js
@@ -9,7 +9,13 @@
  * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
  */
 
-Ext.Loader.setPath('Ext.ux', '/pimcore/static6/js/lib/ext/ux');
+if (coreshop.pimcore.version.versionGreaterThanEqual('5.4.0')) {
+    Ext.Loader.setPath('Ext.ux', '/pimcore/static6/js/lib/ext/ux');
+}
+else {
+    Ext.Loader.setPath('Ext.ux', '/bundles/pimcoreadmin/js/lib/ext/ux');
+}
+
 Ext.require([
     'Ext.ux.form.ItemSelector'
 ]);
