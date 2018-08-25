2aa2e18715fc8ff000064d96d3ea85d17376b68d
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-08-25 18:09:47 +0200

    [Migrations] add small helper program to run coreshop migrations

---
+++ src/CoreShop/Bundle/CoreBundle/Command/MigrateCommand.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\Command;
+
+use CoreShop\Bundle\CoreBundle\Installer\Executor\CommandExecutor;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class MigrateCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('coreshop:migrate')
+            ->setDescription('Execute CoreShop migrations.')
+            ->setHelp(
+                <<<EOT
+The <info>%command.name%</info> executes all CoreShop migrations.
+EOT
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $application = $this->getApplication();
+        $application->setCatchExceptions(false);
+
+        $commandExecutor = new CommandExecutor($input, $output, $application);
+        $commandExecutor->runCommand('pimcore:migrations:migrate', ['--bundle' => 'CoreShopCoreBundle'], $output);
+
+        return 0;
+    }
+}
