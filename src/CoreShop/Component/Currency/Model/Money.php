7314a18bdf689ec02816a07e11d68cf217496751
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-10-02 12:02:27 +0200

    introduce a new money-currency type to store money-values with a currency

---
+++ src/CoreShop/Component/Currency/Model/Money.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Component\Currency\Model;
+
+class Money
+{
+    /**
+     * @var int
+     */
+    public $value;
+
+    /**
+     * @var CurrencyInterface
+     */
+    public $currency;
+
+    /**
+     * @param int               $value
+     * @param CurrencyInterface $currency
+     */
+    public function __construct(int $value, CurrencyInterface $currency)
+    {
+        $this->value = $value;
+        $this->currency = $currency;
+    }
+
+    /**
+     * @return int
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param int $value
+     */
+    public function setValue(int $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return CurrencyInterface
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param CurrencyInterface $currency
+     */
+    public function setCurrency(CurrencyInterface $currency)
+    {
+        $this->currency = $currency;
+    }
+}
