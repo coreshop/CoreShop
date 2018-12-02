7b63515ea6a0e9803f3f2719191feb9ae5129fb5
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2018-12-02 11:28:53 +0100

    [All] update copyright headers

--- src/CoreShop/Bundle/CurrencyBundle/Resources/public/pimcore/js/coreExtension/data/coreShopCurrency.js
+++ src/CoreShop/Bundle/CurrencyBundle/Resources/public/pimcore/js/coreExtension/data/coreShopCurrency.js
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
  * files that are distributed with this source code.
  *
- * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
  * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
  *
  */
