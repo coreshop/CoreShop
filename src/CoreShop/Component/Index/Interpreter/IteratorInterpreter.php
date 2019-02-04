9d58e789fe995be886ad300142db8499e66a8702
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-02-04 11:44:19 +0100

    [Index] introduce iterator interpreter

---
+++ src/CoreShop/Component/Index/Interpreter/IteratorInterpreter.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Component\Index\Interpreter;
+
+use CoreShop\Component\Index\Model\IndexableInterface;
+use CoreShop\Component\Index\Model\IndexColumnInterface;
+use CoreShop\Component\Registry\ServiceRegistryInterface;
+use Webmozart\Assert\Assert;
+
+final class IteratorInterpreter implements InterpreterInterface
+{
+    /**
+     * @var ServiceRegistryInterface
+     */
+    private $interpreterRegistry;
+
+    /**
+     * @param ServiceRegistryInterface $interpreterRegistry
+     */
+    public function __construct(ServiceRegistryInterface $interpreterRegistry)
+    {
+        $this->interpreterRegistry = $interpreterRegistry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function interpret($value, IndexableInterface $indexable, IndexColumnInterface $config, $interpreterConfig = [])
+    {
+        Assert::isArray($value, 'IteratorInterpreter can only be used with array values');
+
+        $interpreter = $interpreterConfig['interpreter'];
+        $interpreterObject = $this->interpreterRegistry->get($interpreter['type']);
+
+        foreach ($value as &$val) {
+            $val = $interpreterObject->interpret($val, $indexable, $config, $interpreter['interpreterConfig']);
+        }
+
+        return $value;
+    }
+}
