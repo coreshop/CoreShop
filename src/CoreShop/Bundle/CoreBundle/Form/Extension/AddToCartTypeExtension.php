7d0963eab2afdb2ef2d60e80921225a8de64ea3d
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-03-08 16:24:48 +0100

    twig templating, cart extension

---
+++ src/CoreShop/Bundle/CoreBundle/Form/Extension/AddToCartTypeExtension.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\Form\Extension;
+
+use CoreShop\Bundle\OrderBundle\Form\Type\AddToCartType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class AddToCartTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('unitDefinition', ChoiceType::class, [
+            'mapped'   => false,
+            'required' => false,
+            'label'    => null,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return AddToCartType::class;
+    }
+}
---
+++ src/CoreShop/Bundle/ProductBundle/Templating/Helper/ProductUnitDefinitionsHelper.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\ProductBundle\Templating\Helper;
+
+use CoreShop\Component\Core\Context\ShopperContextInterface;
+use CoreShop\Component\Product\Model\ProductInterface;
+use CoreShop\Component\Product\Model\ProductUnitDefinitionsInterface;
+use Symfony\Component\Templating\Helper\Helper;
+
+class ProductUnitDefinitionsHelper extends Helper implements ProductUnitDefinitionsHelperInterface
+{
+    /**
+     * @var ShopperContextInterface
+     */
+    protected $shopperContext;
+
+    /**
+     * @param ShopperContextInterface $shopperContext
+     */
+    public function __construct(
+        ShopperContextInterface $shopperContext
+    ) {
+        $this->shopperContext = $shopperContext;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasAvailableUnitDefinitions(ProductInterface $product)
+    {
+        $productUnitDefinitions = $product->getUnitDefinitions();
+        return $productUnitDefinitions instanceof ProductUnitDefinitionsInterface;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'coreshop_product_unit_definitions';
+    }
+}
