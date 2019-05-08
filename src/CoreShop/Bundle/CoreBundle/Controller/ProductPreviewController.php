20d7f5372a3115313aef8cbfd2279b038e0d7274
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-05-08 11:56:35 +0200

    [CoreBundle] introduce store-preview for products

---
+++ src/CoreShop/Bundle/CoreBundle/Controller/ProductPreviewController.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\Controller;
+
+use CoreShop\Bundle\ResourceBundle\Controller\AdminController;
+use CoreShop\Component\Core\Model\ProductInterface;
+use CoreShop\Component\Store\Model\StoreInterface;
+use Pimcore\Model\Site;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Pimcore\Tool;
+use Pimcore\Model\DataObject;
+
+class ProductPreviewController extends AdminController
+{
+    public function previewAction(Request $request)
+    {
+        if (!$request->get('store')) {
+            return new Response('No Store selected');
+        }
+
+        $store = $this->get('coreshop.repository.store')->find($request->get('store'));
+
+        if (!$store instanceof StoreInterface) {
+            return new Response('Invalid Store selected');
+        }
+
+        $site = null;
+
+        if (!$store->getIsDefault()) {
+            $site = Site::getById($store->getSiteId());
+
+            if (!$site) {
+                return new Response('Store\'s Site is invalid');
+            }
+        }
+
+        $object = $this->get('coreshop.repository.product')->find($request->get('id'));
+
+        if (!$object instanceof ProductInterface) {
+            return new Response('Store Preview is only available for Products');
+        }
+
+        if (!in_array($store->getId(), array_values($object->getStores()))) {
+            return new Response('Selected Store is Invalid for Product');
+        }
+
+        $url = $object->getClass()->getPreviewUrl();
+        if ($url) {
+            // replace named variables
+            $vars = $object->getObjectVars();
+            foreach ($vars as $key => $value) {
+                if (!empty($value) && (is_string($value) || is_numeric($value))) {
+                    $url = str_replace('%' . $key, urlencode($value), $url);
+                } else {
+                    if (strpos($url, '%' . $key) !== false) {
+                        return new Response('No preview available, please ensure that all fields which are required for the preview are filled correctly.');
+                    }
+                }
+            }
+        } elseif ($linkGenerator = $object->getClass()->getLinkGenerator()) {
+            $url = $linkGenerator->generate($object, ['preview' => true, 'context' => $this]);
+        }
+
+        if (!$url) {
+            return new Response("Preview not available, it seems that there's a problem with this object.");
+        }
+
+        // replace all remainaing % signs
+        $url = str_replace('%', '%25', $url);
+
+        $urlParts = parse_url($url);
+
+        return $this->redirect(($site ? 'https://' . $site->getMainDomain() : '') . $urlParts['path']);
+    }
+}
