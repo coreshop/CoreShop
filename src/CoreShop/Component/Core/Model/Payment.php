4de3933e57335d61281decfcdd4636ae5198f509
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-06-05 16:44:51 +0200

    [Payment] refactor payment to not depend directly on payum models

--- src/CoreShop/Component/Core/Model/Payment.php
+++ src/CoreShop/Component/Core/Model/Payment.php
@@ -23,6 +23,11 @@ class Payment extends BasePayment implements PaymentInterface
     protected $order;
 
     /**
+     * @var CurrencyInterface
+     */
+    protected $currency;
+
+    /**
      * {@inheritdoc}
      */
     public function getOrder()
@@ -39,5 +44,22 @@ class Payment extends BasePayment implements PaymentInterface
 
         $this->order = $order;
         $this->orderId = $order->getId();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCurrency($currency)
+    {
+        $this->currencyCode = $currency->getIsoCode();
+        $this->currency = $currency;
     }
 }
