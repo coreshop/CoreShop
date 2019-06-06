fd16b25dda290fdec2c5cb0f5f128c7ee61ea319
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-06-06 12:40:34 +0200

    [Symfony4] introduce AutoMapping Validator fix until the issue is actually sorted

---
+++ src/CoreShop/Bundle/ResourceBundle/DependencyInjection/Compiler/ValidatorAutoMappingFixPass.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\ResourceBundle\DependencyInjection\Compiler;
+
+use CoreShop\Component\Resource\Model\ResourceInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+
+final class ValidatorAutoMappingFixPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasParameter('validator.auto_mapping')) {
+            return;
+        }
+
+        $autoMapping = $container->getParameter('validator.auto_mapping');
+
+        if ([] !== $autoMapping) {
+            return;
+        }
+
+        $container->setParameter('validator.auto_mapping', [
+            'AppBundle\Entity' => [
+                'services' => []
+            ]
+        ]);
+    }
+}
