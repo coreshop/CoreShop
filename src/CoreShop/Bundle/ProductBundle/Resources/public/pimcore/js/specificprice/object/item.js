b51f2d37b066c0a8f2dc70c941da3bf1433e7e0a
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-07-12 08:43:19 +0200

    [ProductBundle] specific price rules: keep id on save

--- src/CoreShop/Bundle/ProductBundle/Resources/public/pimcore/js/specificprice/object/item.js
+++ src/CoreShop/Bundle/ProductBundle/Resources/public/pimcore/js/specificprice/object/item.js
@@ -16,6 +16,11 @@ coreshop.product.specificprice.object.item = Class.create(coreshop.rules.item, {
 
     iconCls: 'coreshop_icon_price_rule',
 
+    postSaveObject: function (object, refreshedRuleData, task, fieldName) {
+        // remove dirty flag!
+        //this.settingsForm.getForm().setValues(this.settingsForm.getForm().getValues());
+    },
+
     getPanel: function () {
         this.panel = new Ext.TabPanel({
             activeTab: 0,
@@ -120,6 +125,10 @@ coreshop.product.specificprice.object.item = Class.create(coreshop.rules.item, {
         }
 
         return {};
+    },
+
+    getId: function () {
+        return this.data.id ? this.data.id : null;
     },
 
     isDirty: function () {
