d1d49ec362e7c956847b2d21ec8a2a752be89cf1
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-07-24 14:27:34 +0200

    [IndexBundle] fix iterator and nested interpreter

--- src/CoreShop/Bundle/IndexBundle/Resources/public/pimcore/js/index/interpreters/iterator.js
+++ src/CoreShop/Bundle/IndexBundle/Resources/public/pimcore/js/index/interpreters/iterator.js
@@ -15,9 +15,7 @@ pimcore.registerNS('coreshop.index.interpreters.iterator');
 
 coreshop.index.interpreters.iterator = Class.create(coreshop.index.interpreters.abstract, {
     getStore: function() {
-        return {
-            type: 'coreshop_index_interpreters'
-        }
+        return pimcore.globalmanager.get('coreshop_index_interpreters');
     },
 
     getClassItem: function() {
