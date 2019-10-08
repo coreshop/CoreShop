32fdb767b93b09d781bcdcc1467ade2a9a01dce5
David Höck <d.hoeck@alpin11.at>
2019-10-08 13:30:06 +0200

    updated migration

--- src/CoreShop/Bundle/CoreBundle/Migrations/Version20191007174441.php
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20191007174441.php
@@ -5,37 +5,16 @@ namespace CoreShop\Bundle\CoreBundle\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20191007174441 extends AbstractPimcoreMigration
 {
     /**
      * @param Schema $schema
-     *
-     * @throws \Doctrine\DBAL\Schema\SchemaException
      */
     public function up(Schema $schema)
     {
 
         if ($schema->hasTable('coreshop_payment_provider')) {
-
-            $table = $schema->getTable('coreshop_payment_provider');
-
-            if ($table->hasColumn('logo')) {
-                $table->dropColumn('logo');
-            }
-
-            if ($table->hasColumn('logoId')) {
-                $table->dropColumn('logoId');
-            }
-
-            $table->addColumn('logo', 'integer', [
-                'length' => 11,
-                'comment' => '(DC2Type:pimcoreAsset)',
-                'notnull' => false
-            ]);
-
+            $this->addSql('ALTER TABLE coreshop_payment_provider CHANGE logoId logoId INT DEFAULT NULL COMMENT `(DC2Type:pimcoreAsset)`');
         }
     }
 
