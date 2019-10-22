bb1a4041d4fd91bc841eed2914d7c116258c1bb4
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-10-22 14:34:36 +0200

    check for invalid inherited data, see #1137

---
+++ src/CoreShop/Bundle/CoreBundle/EventListener/QuantityRangeUnitValidationListener.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\EventListener;
+
+use CoreShop\Component\Core\Model\ProductInterface;
+use CoreShop\Component\Product\Model\ProductUnitDefinitionInterface;
+use CoreShop\Component\Product\Model\ProductUnitDefinitionsInterface;
+use CoreShop\Component\Resource\Model\AbstractObject;
+use CoreShop\Component\Resource\Repository\RepositoryInterface;
+use CoreShop\Bundle\ProductQuantityPriceRulesBundle\Event\ProductQuantityPriceRuleValidationEvent;
+use Pimcore\Model\Element\ValidationException;
+
+final class QuantityRangeUnitValidationListener
+{
+    /**
+     * @var RepositoryInterface
+     */
+    protected $productUnitDefinitionRepository;
+
+    /**
+     * @param RepositoryInterface $productUnitDefinitionRepository
+     */
+    public function __construct(RepositoryInterface $productUnitDefinitionRepository)
+    {
+        $this->productUnitDefinitionRepository = $productUnitDefinitionRepository;
+    }
+
+    /**
+     * @param ProductQuantityPriceRuleValidationEvent $event
+     *
+     * @throws ValidationException
+     */
+    public function validate(ProductQuantityPriceRuleValidationEvent $event)
+    {
+        $object = $event->getObject();
+        $data = $event->getData();
+
+        if (!$object instanceof ProductInterface) {
+            return;
+        }
+
+        // this listener only validates variant data
+        if ($object->getType() !== AbstractObject::OBJECT_TYPE_VARIANT) {
+            return;
+        }
+
+        if (!is_array($data)) {
+            return;
+        }
+
+        foreach ($data as $rule) {
+            $this->validateRule($rule, $object);
+        }
+    }
+
+    /**
+     * @param array            $rule
+     * @param ProductInterface $product
+     *
+     * @throws ValidationException
+     */
+    protected function validateRule(array $rule, ProductInterface $product)
+    {
+        if (!isset($rule['ranges']) || !is_array($rule['ranges'])) {
+            return;
+        }
+
+        $ranges = $rule['ranges'];
+
+        foreach ($ranges as $range) {
+
+            if (!isset($range['unitDefinition']) || !is_int($range['unitDefinition'])) {
+                continue;
+            }
+
+            $unitDefinitionId = $range['unitDefinition'];
+
+            $unitDefinition = $this->productUnitDefinitionRepository->find($unitDefinitionId);
+            if (!$unitDefinition instanceof ProductUnitDefinitionInterface) {
+                continue;
+            }
+
+            $productUnitDefinitions = $unitDefinition->getProductUnitDefinitions();
+            if (!$productUnitDefinitions instanceof ProductUnitDefinitionsInterface) {
+                continue;
+            }
+
+            $unitDefinitionProduct = $productUnitDefinitions->getProduct();
+            if (!$unitDefinitionProduct instanceof ProductInterface) {
+                continue;
+            }
+
+            if ($unitDefinitionProduct->getId() !== $product->getId()) {
+                throw new ValidationException('Invalid unit definition reference. please reload your object, define unit definition first and blabla.');
+            }
+        }
+    }
+}
