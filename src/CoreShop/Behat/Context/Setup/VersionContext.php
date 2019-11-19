af173784829ad8ec5ecf272153442b35da30ee2e
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-11-19 16:00:55 +0100

    [Behat] add Version Tests

---
+++ src/CoreShop/Behat/Context/Setup/VersionContext.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Behat\Context\Setup;
+
+use Behat\Behat\Context\Context;
+use CoreShop\Behat\Service\SharedStorageInterface;
+use CoreShop\Component\Address\Model\ZoneInterface;
+use CoreShop\Component\Pimcore\DataObject\VersionHelper;
+use CoreShop\Component\Resource\Factory\FactoryInterface;
+use CoreShop\Component\Resource\Repository\RepositoryInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\Version;
+
+final class VersionContext implements Context
+{
+    /**
+     * @var SharedStorageInterface
+     */
+    private $sharedStorage;
+
+    /**
+     * @param SharedStorageInterface $sharedStorage
+     */
+    public function __construct(
+        SharedStorageInterface $sharedStorage
+    ) {
+        $this->sharedStorage = $sharedStorage;
+    }
+
+    /**
+     * @Then /^I remember the (product "[^"]+") Version$/
+     * @Then /^I remember the (product) Version$/
+     */
+    public function iRememberTheProductVersion(Concrete $concrete)
+    {
+        $concrete->saveVersion();
+
+        $this->sharedStorage->set('data_object_version_' . $concrete->getId(), $concrete->getLatestVersion()->getId());
+    }
+
+    /**
+     * @Then /^I restore the remembered (product) Version$/
+     */
+    public function iRestoreTheRememberedProductVersion(Concrete $concrete)
+    {
+        $key = 'data_object_version_' . $concrete->getId();
+
+        $data = $this->restoreVersion($concrete, $key);
+
+        $data->save();
+
+        $this->sharedStorage->set('product', $data);
+    }
+
+    protected function restoreVersion(Concrete $concrete, $key)
+    {
+        if (!$this->sharedStorage->has($key)) {
+            throw new \InvalidArgumentException('No Version remembered');
+        }
+
+        $version = Version::getById($this->sharedStorage->get($key));
+
+        if (!$version) {
+            throw new \InvalidArgumentException('Version does not exist');
+        }
+
+        $data = $version->loadData();
+
+        if (!$data instanceof $concrete) {
+            throw new \InvalidArgumentException('Version Type and Object Type do not match');
+        }
+
+        return $data;
+    }
+}
