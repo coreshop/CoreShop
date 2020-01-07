44e2b6c9c97bb0249e76a50a758782011732d91c
Lukas Huppertz <lukas.huppertz@snk.de>
2020-01-07 09:25:45 +0100

    GH-1209 Add Deprecations + add deprecation to AddToCartQuantity + add deprecation to CartQuantity

---
+++ src/CoreShop/Bundle/CoreBundle/Validator/Constraints/AddToCartQuantity.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2019 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
+
+namespace CoreShop\Bundle\CoreBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @deprecated AddToCartQuantity is deprecated use AddToCartMinimumQuantity instead
+ */
+final class AddToCartQuantity extends Constraint
+{
+    /**
+     * @var string
+     */
+    public $messageBelowMinimum;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy(): string
+    {
+        @trigger_error(
+            'Calling AddToCartQuantity is deprecated use AddToCartMinimumQuantity instead',
+            E_USER_DEPRECATED
+        );
+        return 'coreshop_add_to_cart_quantity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets(): string
+    {
+        @trigger_error(
+            'Calling AddToCartQuantity is deprecated use AddToCartMinimumQuantity instead',
+            E_USER_DEPRECATED
+        );
+        return self::CLASS_CONSTRAINT;
+    }
+}
