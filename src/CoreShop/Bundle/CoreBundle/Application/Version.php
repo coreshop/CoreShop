b978592ea05a860a0a82ba6958763ae30594f428
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-05-04 10:15:53 +0200

    [2.1.7] prepare release

--- src/CoreShop/Bundle/CoreBundle/Application/Version.php
+++ src/CoreShop/Bundle/CoreBundle/Application/Version.php
@@ -16,7 +16,7 @@ final class Version
 {
     const MAJOR_VERSION = '2';
     const MINOR_VERSION = '1';
-    const RELEASE_VERSION = '6';
+    const RELEASE_VERSION = '7';
     const EXTRA_VERSION = '';
 
     /**
