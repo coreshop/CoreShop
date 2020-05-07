861e23f5c724b564c4dd70e0e12c3f5c67298324
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-05-07 09:42:21 +0200

    [FrontendBundle] revert url-forward, doesn't work for all cases

--- src/CoreShop/Bundle/FrontendBundle/Controller/ProductController.php
+++ src/CoreShop/Bundle/FrontendBundle/Controller/ProductController.php
@@ -58,12 +58,6 @@ class ProductController extends FrontendController
             throw new NotFoundHttpException('product not found');
         }
 
-        $urlToBe = $this->get(LinkGeneratorInterface::class)->generate($product);
-
-        if (urldecode($request->getPathInfo()) !== $urlToBe) {
-            return $this->redirect($urlToBe);
-        }
-
         if (!in_array($this->get('coreshop.context.store')->getStore()->getId(), $product->getStores())) {
             throw new NotFoundHttpException('product not found');
         }
