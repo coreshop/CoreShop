20239ccaa48bb394b2daa685e793b059f691b31e
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-09-17 14:21:54 +0200

    fix pseudo price view in qpr, fixes #1488

--- src/CoreShop/Bundle/CoreBundle/Resources/public/pimcore/js/productquantitypricerules/ranges.js
+++ src/CoreShop/Bundle/CoreBundle/Resources/public/pimcore/js/productquantitypricerules/ranges.js
@@ -284,10 +284,9 @@ coreshop.product_quantity_price_rules.ranges = Class.create(coreshop.product_qua
                     }
 
                     if (value === undefined) {
-                        return coreshop.util.format.currency('', 0);
+                        return '0';
                     } else {
-
-                        return coreshop.util.format.currency('', parseFloat(value) * pimcore.globalmanager.get('coreshop.currency.decimal_factor'));
+                        return coreshop.util.format.number(parseFloat(value * pimcore.globalmanager.get('coreshop.currency.decimal_factor')));
                     }
                 }
             }]
