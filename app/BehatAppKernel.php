3259c8cc8fe5d71731069183ec0dc2f42487bfff
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-11-12 22:44:35 +0100

    [Behat] fixes

--- app/BehatAppKernel.php
+++ app/BehatAppKernel.php
@@ -33,6 +33,14 @@ class BehatAppKernel extends Kernel
         \Pimcore::setKernel($this);
     }
 
+    protected function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->setParameter('pimcore.geoip.db_file', $container->getParameter('kernel.project_dir') . '/var/config/GeoLite2-City.mmdb');
+    }
+
+
     protected function getEnvironmentsForDevBundles(): array
     {
         return array_merge(
