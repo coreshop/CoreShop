02ef169af88c95e450787bb3b461584d54f79e4a
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-05-11 09:26:58 +0200

    [FrontendBundle] clear password reset hash after reset

--- src/CoreShop/Bundle/FrontendBundle/Controller/RegisterController.php
+++ src/CoreShop/Bundle/FrontendBundle/Controller/RegisterController.php
@@ -142,6 +142,7 @@ class RegisterController extends FrontendController
                 if ($handledForm->isSubmitted() && $handledForm->isValid()) {
                     $resetPassword = $handledForm->getData();
 
+                    $customer->setPasswordResetHash(null);
                     $customer->setPassword($resetPassword['password']);
                     $customer->save();
 
