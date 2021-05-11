b98204c2bcbc76a88a4895a2bf9051daf5eaecd5
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-05-11 09:47:26 +0200

    [Payment] make payment description translateable

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20210511074115.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use CoreShop\Component\Pimcore\Migration\SharedTranslation;
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+
+class Version20210511074115 extends AbstractPimcoreMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        SharedTranslation::add('coreshop.order_payment.total', 'de', 'Zahlung behinhaltet %items% Einträge für Betrag %total%.');
+        SharedTranslation::add('coreshop.order_payment.total', 'de_CH', 'Zahlung behinhaltet %items% Einträge für Betrag %total%.');
+        SharedTranslation::add('coreshop.order_payment.total', 'en', 'Payment contains %items% item(s) for a total of %total%.');
+        SharedTranslation::add('coreshop.order_payment.total', 'it', 'Payment contains %items% item(s) for a total of %total%.');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
