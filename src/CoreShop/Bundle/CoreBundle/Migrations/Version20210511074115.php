2d88e3bea73f0d6b83bebd27e4303eddcf1dcb39
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-06-29 14:44:44 +0200

    [CoreBundle] fix migration

--- src/CoreShop/Bundle/CoreBundle/Migrations/Version20210511074115.php
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20210511074115.php
@@ -4,14 +4,14 @@ namespace CoreShop\Bundle\CoreBundle\Migrations;
 
 use CoreShop\Component\Pimcore\Migration\SharedTranslation;
 use Doctrine\DBAL\Schema\Schema;
-use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Doctrine\Migrations\AbstractMigration;
 
-class Version20210511074115 extends AbstractPimcoreMigration
+class Version20210511074115 extends AbstractMigration
 {
     /**
      * @param Schema $schema
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         SharedTranslation::add('coreshop.order_payment.total', 'de', 'Zahlung behinhaltet %items% Einträge für Betrag %total%.');
         SharedTranslation::add('coreshop.order_payment.total', 'de_CH', 'Zahlung behinhaltet %items% Einträge für Betrag %total%.');
@@ -22,7 +22,7 @@ class Version20210511074115 extends AbstractPimcoreMigration
     /**
      * @param Schema $schema
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
 
