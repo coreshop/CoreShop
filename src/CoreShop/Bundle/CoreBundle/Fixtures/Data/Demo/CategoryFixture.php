2bd57b26cd3701ae280e05902355d0a4e3efc40f
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-07-15 13:22:05 +0200

    [Slug] default generate slugs and use instead of static routes for product and category - change fixtures to more predictable create category and product names

--- src/CoreShop/Bundle/CoreBundle/Fixtures/Data/Demo/CategoryFixture.php
+++ src/CoreShop/Bundle/CoreBundle/Fixtures/Data/Demo/CategoryFixture.php
@@ -14,13 +14,11 @@ declare(strict_types=1);
 
 namespace CoreShop\Bundle\CoreBundle\Fixtures\Data\Demo;
 
-use CoreShop\Bundle\CoreBundle\Faker\Commerce;
 use CoreShop\Bundle\FixtureBundle\Fixture\VersionedFixtureInterface;
 use CoreShop\Component\Core\Model\CategoryInterface;
 use CoreShop\Component\Pimcore\DataObject\ObjectServiceInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 use Pimcore\Model\DataObject\Service;
 use Pimcore\Tool;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
@@ -42,10 +40,31 @@ class CategoryFixture extends AbstractFixture implements ContainerAwareInterface
 
     public function load(ObjectManager $manager): void
     {
+        $names = [
+            [
+                'de' => 'Bücher',
+                'en' => 'Books',
+            ],
+            [
+                'de' => 'Computer',
+                'en' => 'Computer',
+            ],
+            [
+                'de' => 'Filme',
+                'en' => 'Movies',
+            ],
+            [
+                'de' => 'Kleidung',
+                'en' => 'Clothing',
+            ],
+            [
+                'de' => 'Schuhe',
+                'en' => 'Shoes',
+            ],
+        ];
+
         if (!count($this->container->get('coreshop.repository.category')->findAll())) {
             $categoriesCount = 5;
-            $faker = Factory::create();
-            $faker->addProvider(new Commerce($faker));
 
             for ($i = 0; $i < $categoriesCount; $i++) {
                 /**
@@ -54,7 +73,7 @@ class CategoryFixture extends AbstractFixture implements ContainerAwareInterface
                 $category = $this->container->get('coreshop.factory.category')->createNew();
 
                 foreach (Tool::getValidLanguages() as $language) {
-                    $category->setName($faker->department, $language);
+                    $category->setName($names[$i][$language] ?? $names[$i]['en'], $language);
                 }
 
                 $category->setParent($this->container->get(ObjectServiceInterface::class)->createFolderByPath('/demo/categories'));
