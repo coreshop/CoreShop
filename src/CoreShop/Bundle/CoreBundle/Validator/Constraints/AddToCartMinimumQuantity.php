3c608e2164e7aa903e68992311d7402d19890e7a
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-07-22 08:49:58 +0200

    [All] Update Copyrights to CoreShop GmbH

--- src/CoreShop/Bundle/CoreBundle/Validator/Constraints/AddToCartMinimumQuantity.php
+++ src/CoreShop/Bundle/CoreBundle/Validator/Constraints/AddToCartMinimumQuantity.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
  * files that are distributed with this source code.
  *
- * @copyright  Copyright (c) 2015-2021 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.org)
  * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
  */
 
@@ -16,7 +16,7 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * TODO 3.0.0: uncomment final
- * 
+ *
  * @final DO NOT EXTEND, THIS IS TEMPORARY NOT FINAL
  */
 /*final*/ class AddToCartMinimumQuantity extends Constraint
