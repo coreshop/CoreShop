070b10c53a3dfa73f9731ff3a6d5f8c8ade0f678
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-07-22 08:58:59 +0200

    [All] Update Copyrights to CoreShop GmbH

--- src/CoreShop/Bundle/IndexBundle/Resources/public/pimcore/js/index/interpreters/nested.js
+++ src/CoreShop/Bundle/IndexBundle/Resources/public/pimcore/js/index/interpreters/nested.js
@@ -5,7 +5,7 @@
  * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
  * files that are distributed with this source code.
  *
- * @copyright  Copyright (c) 2015-2021 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.org)
  * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
  *
  */
@@ -71,7 +71,7 @@ coreshop.index.interpreters.nested = Class.create(coreshop.index.interpreters.ab
     addInterpreter: function (type, record, config) {
         // create condition
         type = type.toLowerCase();
-        
+
         var interpreterClass = this.getInterpreterClassItem(type);
         var item = new interpreterClass();
         var container = new coreshop.index.interpreters.nestedcontainer(this, type, item);
