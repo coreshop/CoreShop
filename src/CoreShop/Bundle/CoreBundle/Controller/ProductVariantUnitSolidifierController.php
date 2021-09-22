7ae9b4f49db6a318b00719358db9b77aae5d6d52
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-09-22 21:55:52 +0200

    [Core] revert adding getChildren

--- src/CoreShop/Bundle/CoreBundle/Controller/ProductVariantUnitSolidifierController.php
+++ src/CoreShop/Bundle/CoreBundle/Controller/ProductVariantUnitSolidifierController.php
@@ -35,6 +35,7 @@ class ProductVariantUnitSolidifierController extends AdminController
 
     public function checkStatusAction(Request $request, int $objectId): Response
     {
+        /** @var DataObject\Concrete $object */
         $object = $this->getProductRepository()->find($objectId);
 
         if (!$object instanceof ProductInterface) {
@@ -47,10 +48,6 @@ class ProductVariantUnitSolidifierController extends AdminController
         $strategy = null;
         $errorStatus = false;
 
-        /**
-         * @var DataObject\Concrete $object
-         * @var ProductInterface $object
-         */
         $variants = $object->getChildren([AbstractObject::OBJECT_TYPE_VARIANT], true);
 
         if (count($variants) === 0) {
