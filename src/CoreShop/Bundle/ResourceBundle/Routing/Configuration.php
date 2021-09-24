73e7d84c31b30daa7d72723992be30a681c1cd96
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-09-24 20:39:54 +0200

    [Psalm] fixes and refactorings

--- src/CoreShop/Bundle/ResourceBundle/Routing/Configuration.php
+++ src/CoreShop/Bundle/ResourceBundle/Routing/Configuration.php
@@ -23,7 +23,7 @@ final class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('routing');
-        /** @var ArrayNodeDefinition $rootNode */
+        /** @psalm-var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
