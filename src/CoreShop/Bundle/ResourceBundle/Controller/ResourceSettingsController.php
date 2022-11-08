85e0af087191e28baf51e89da9c18c6fb999d29a
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2022-11-08 11:24:02 +0100

    [ResourceBundle] class names with lower-case names

--- src/CoreShop/Bundle/ResourceBundle/Controller/ResourceSettingsController.php
+++ src/CoreShop/Bundle/ResourceBundle/Controller/ResourceSettingsController.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 
 namespace CoreShop\Bundle\ResourceBundle\Controller;
 
+use CoreShop\Bundle\ResourceBundle\CoreShopResourceBundle;
 use Pimcore\Model\Element\AbstractElement;
 use Pimcore\Model\Element\Service;
 use Symfony\Component\HttpFoundation\Request;
@@ -55,15 +56,23 @@ class ResourceSettingsController extends AdminController
             $classes = $this->container->getParameter('coreshop.all.pimcore_classes');
 
             foreach ($classes as $key => $definition) {
+                if (!isset($definition['classes']['type'])) {
+                    continue;
+                }
+
+                if ($definition['classes']['type'] !== CoreShopResourceBundle::PIMCORE_MODEL_TYPE_OBJECT) {
+                    continue;
+                }
+
                 $alias = explode('.', $key);
                 $application = $alias[0];
                 $alias = $alias[1];
 
-                if (isset($definition['classes']['model'])) {
+                if (isset($definition['classes']['pimcore_class_name'])) {
                     $class = $definition['classes']['pimcore_class_name'];
                 }
                 else {
-                    $fullClassName = $definition['classes']['pimcore_class_name'];
+                    $fullClassName = $definition['classes']['model'];
                     $class = str_replace(['Pimcore\\Model\\DataObject\\', '\\'], '', $fullClassName);
                 }
 
