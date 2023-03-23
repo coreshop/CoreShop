b47845f257ee903648388d07d255fbb4f4ab1556
Aleksey Ranuk <ar@snov.io>
2023-03-23 12:22:25 +0200

    fix when $values is empty array

--- src/CoreShop/Component/Index/Filter/SelectFilterConditionFromMultiselectProcessor.php
+++ src/CoreShop/Component/Index/Filter/SelectFilterConditionFromMultiselectProcessor.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 
 namespace CoreShop\Component\Index\Filter;
 
+use CoreShop\Component\Index\Condition\InCondition;
 use CoreShop\Component\Index\Condition\LikeCondition;
 use CoreShop\Component\Index\Listing\ListingInterface;
 use CoreShop\Component\Index\Model\FilterConditionInterface;
@@ -87,17 +88,18 @@ class SelectFilterConditionFromMultiselectProcessor implements FilterConditionPr
         }
 
         if (!empty($value)) {
-            $value = ',' . $value . ',';
 
-            $currentFilter[$field] = $value;
+            $fieldName = $isPrecondition ? 'PRECONDITION_' . $field : $field;
 
-            $fieldName = $field;
+            if (is_array($value)) {
+                $list->addCondition(new InCondition($field, $value), $fieldName);
+            } else {
+                $value = ',' . $value . ',';
 
-            if ($isPrecondition) {
-                $fieldName = 'PRECONDITION_' . $fieldName;
+                $currentFilter[$field] = $value;
+
+                $list->addCondition(new LikeCondition($field, 'both', $value), $fieldName);
             }
-
-            $list->addCondition(new LikeCondition($field, 'both', $value), $fieldName);
         }
 
         return $currentFilter;
