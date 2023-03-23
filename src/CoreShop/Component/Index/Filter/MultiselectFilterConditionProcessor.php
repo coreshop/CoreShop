fae74aab474a9cc0b10a9898dc00513250d0a1d8
Aleksey Ranuk <ar@snov.io>
2023-03-23 12:06:07 +0200

    fix when $values is empty array

--- src/CoreShop/Component/Index/Filter/MultiselectFilterConditionProcessor.php
+++ src/CoreShop/Component/Index/Filter/MultiselectFilterConditionProcessor.php
@@ -53,7 +53,7 @@ class MultiselectFilterConditionProcessor implements FilterConditionProcessorInt
 
         $currentFilter[$field] = $values;
 
-        if ($values === static::EMPTY_STRING || [] === $values) {
+        if ($values === static::EMPTY_STRING) {
             $values = null;
         }
 
