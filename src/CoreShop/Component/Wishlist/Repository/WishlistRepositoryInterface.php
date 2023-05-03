a7a274f2215bb52266ed43456c7882dec460bf7c
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2023-05-03 12:11:34 +0200

    [WishlistBundle] make wishlist easier usable outside of CoreShop - remove expire

--- src/CoreShop/Component/Wishlist/Repository/WishlistRepositoryInterface.php
+++ src/CoreShop/Component/Wishlist/Repository/WishlistRepositoryInterface.php
@@ -18,12 +18,11 @@ declare(strict_types=1);
 
 namespace CoreShop\Component\Wishlist\Repository;
 
-use CoreShop\Component\StorageList\Repository\ExpireAbleStorageListRepositoryInterface;
 use CoreShop\Component\StorageList\Repository\PimcoreStorageListRepositoryInterface;
 use CoreShop\Component\StorageList\Repository\ShareableStorageListRepositoryInterface;
 use CoreShop\Component\Wishlist\Model\WishlistInterface;
 
-interface WishlistRepositoryInterface extends PimcoreStorageListRepositoryInterface, ShareableStorageListRepositoryInterface, ExpireAbleStorageListRepositoryInterface
+interface WishlistRepositoryInterface extends PimcoreStorageListRepositoryInterface, ShareableStorageListRepositoryInterface
 {
     public function findByToken(string $token): ?WishlistInterface;
 }
