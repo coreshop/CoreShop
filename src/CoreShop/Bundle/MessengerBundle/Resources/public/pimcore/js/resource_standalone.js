44080180c74e702dcd14963d39d64afc9a94a2ab
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2023-09-22 13:31:15 +0200

    [Menu] fix standalone used menus

--- src/CoreShop/Bundle/MessengerBundle/Resources/public/pimcore/js/resource_standalone.js
+++ src/CoreShop/Bundle/MessengerBundle/Resources/public/pimcore/js/resource_standalone.js
@@ -1,4 +1,3 @@
-
 /*
  * CoreShop.
  *
@@ -14,33 +13,22 @@
 pimcore.registerNS('coreshop.messenger.resource');
 
 if (coreshop.resource === undefined) {
-    coreshop.messenger.resource = Class.create(pimcore.plugin.admin, {
-        initialize: function () {
-            var me = this;
 
-            document.addEventListener(pimcore.events.pimcoreReady, (e) => {
-                if (coreshop.menu.coreshop.messenger) {
-                    new coreshop.menu.coreshop.messenger();
-                }
-            });
+    document.addEventListener(pimcore.events.pimcoreReady, (e) => {
+        if (coreshop.menu.coreshop.messenger) {
+            new coreshop.menu.coreshop.messenger();
+        }
+    });
 
-            document.addEventListener(coreshop.events.menu.open, (e) => {
-                var item = e.detail.item;
-                var type = e.detail.type;
+    document.addEventListener(coreshop.events.menu.open, (e) => {
+        var item = e.detail.item;
 
-                if (type === 'coreshop.messenger' && item.attributes.function === 'list') {
-                    me.openList();
-                }
-            });
-        },
-
-        openList: function () {
+        if (item.attributes.resource === 'coreshop.messenger' && item.attributes.function === 'list') {
             try {
                 pimcore.globalmanager.get('coreshop_messenger_list').activate();
             } catch (e) {
                 pimcore.globalmanager.add('coreshop_messenger_list', new coreshop.messenger.list());
             }
-        },
+        }
     });
-    new coreshop.messenger.resource();
 }
