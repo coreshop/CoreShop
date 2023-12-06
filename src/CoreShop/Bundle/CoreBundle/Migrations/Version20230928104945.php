6511858de4fa9da59ecf29ee5db25e2252d0b9d0
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2023-12-06 11:45:25 +0100

    [CoreBundle] fix migration to add immutable field to order

--- src/CoreShop/Bundle/CoreBundle/Migrations/Version20230928104945.php
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20230928104945.php
@@ -65,7 +65,7 @@ final class Version20230928104945 extends AbstractMigration implements Container
             'width' => '',
         ];
 
-        $classUpdater->insertFieldAfter('unitDefinition', $immutableField);
+        $classUpdater->insertFieldAfter('additionalData', $immutableField);
         $classUpdater->save();
     }
 
