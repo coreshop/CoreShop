b3e5d2bdd2bf6e91859cc578c4953465bd7eae1d
CoreShop <info@coreshop.org>
2024-03-11 02:19:00 +0000

    [CS] Refactor

--- src/CoreShop/Component/Core/Cart/Rule/Applier/CartRuleApplier.php
+++ src/CoreShop/Component/Core/Cart/Rule/Applier/CartRuleApplier.php
@@ -59,19 +59,19 @@ class CartRuleApplier implements CartRuleApplierInterface
         $totalDiscountPossible = 0;
 
         $discountableItems = [];
-        
+
         foreach ($cart->getItems() as $item) {
             if ($item->getTotal() <= 0) {
                 continue;
             }
-            
+
             $discountableItems[] = $item;
         }
-        
+
         if (count($discountableItems) === 0) {
             return;
-        } 
-        
+        }
+
         foreach ($cart->getItems() as $item) {
             $totalAmount[] = $item->getTotal(false);
             $totalDiscountPossible += $item->getTotal($withTax);
@@ -213,7 +213,7 @@ class CartRuleApplier implements CartRuleApplierInterface
                 $cartPriceRuleItem->getCartPriceRule()->getName(),
                 $positive ? $amountGross : (-1 * $amountGross),
                 $positive ? $amountNet : (-1 * $amountNet),
-                true
+                true,
             ));
         }
 
