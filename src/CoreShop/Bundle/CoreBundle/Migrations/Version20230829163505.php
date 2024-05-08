a5e57eec19a03566be528089c1e572f27900132c
Nils Weigel <n.weigel@neusta.de>
2024-05-08 16:37:16 +0200

    use schema->hasTable to prevent creating the table twice

--- src/CoreShop/Bundle/CoreBundle/Migrations/Version20230829163505.php
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20230829163505.php
@@ -30,8 +30,12 @@ final class Version20230829163505 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        if ($schema->hasTable('coreshop_payment_provider_rule_translation')) {
+            return;
+        }
+
         $this->addSql('
-            CREATE TABLE IF NOT EXISTS coreshop_payment_provider_rule_translation (id INT(11) AUTO_INCREMENT NOT NULL, translatable_id INT(11) NOT NULL, label VARCHAR(256) NULL, creationDate DATETIME NOT NULL, modificationDate DATETIME DEFAULT NULL, locale VARCHAR(5), UNIQUE translatable_id_locale (translatable_id, locale), INDEX translatable_id (translatable_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8MB4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB;
+            CREATE TABLE coreshop_payment_provider_rule_translation (id INT(11) AUTO_INCREMENT NOT NULL, translatable_id INT(11) NOT NULL, label VARCHAR(256) NULL, creationDate DATETIME NOT NULL, modificationDate DATETIME DEFAULT NULL, locale VARCHAR(5), UNIQUE translatable_id_locale (translatable_id, locale), INDEX translatable_id (translatable_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8MB4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB;
             ALTER TABLE coreshop_payment_provider_rule_translation ADD CONSTRAINT FK_9A9D1D4B2C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES coreshop_payment_provider_rule (id) ON DELETE CASCADE;
         ');
     }
