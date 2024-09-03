c28918892b1f5199900849b3bd2a24ca61dd5675
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-09-03 21:00:00 +0200

    [Tests] remove PIMCORE_CLASS_DIRECTORY for tests

--- behat-bootstrap.php
+++ behat-bootstrap.php
@@ -23,8 +23,6 @@ if (!defined('TESTS_PATH')) {
     define('TESTS_PATH', __DIR__);
 }
 
-define('PIMCORE_TEST', true);
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
