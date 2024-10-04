d7b4fae3b3e8fa0ebaab0697e62a153863ea1ea4
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-10-04 08:33:31 +0200

    [Pimcore] introduce LocalizedFallbackHelper

---
+++ src/CoreShop/Component/Pimcore/DataObject/LocalizedFallbackHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * CoreShop
+ *
+ * This source file is available under two different licenses:
+ *  - GNU General Public License version 3 (GPLv3)
+ *  - CoreShop Commercial License (CCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.org)
+ * @license    https://www.coreshop.org/license     GPLv3 and CCL
+ *
+ */
+
+namespace CoreShop\Component\Pimcore\DataObject;
+
+use Pimcore\Model\DataObject\Localizedfield;
+
+class LocalizedFallbackHelper
+{
+    /**
+     * @return mixed
+     */
+    public static function useFallback(\Closure $function, bool $use = false): mixed
+    {
+        $backup = Localizedfield::getGetFallbackValues();
+
+        Localizedfield::setGetFallbackValues($use);
+
+        $result = $function();
+
+        Localizedfield::setGetFallbackValues($backup);
+
+        return $result;
+    }
+}
