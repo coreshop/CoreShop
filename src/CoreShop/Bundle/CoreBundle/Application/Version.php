ee3c2848070021d3856ada96631da7c34325a24b
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-10-16 11:46:33 +0200

    [Release] 3.2.14

--- src/CoreShop/Bundle/CoreBundle/Application/Version.php
+++ src/CoreShop/Bundle/CoreBundle/Application/Version.php
@@ -24,7 +24,7 @@ final class Version
 
     public const MINOR_VERSION = '2';
 
-    public const RELEASE_VERSION = '13';
+    public const RELEASE_VERSION = '14';
 
     public const EXTRA_VERSION = '';
 
