26263f2cdde602b76315ac3e43f9f0ce6f8af7de
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2024-10-18 08:14:00 +0200

    [FrontendBundle] add migration that enables the FrontendBundle again

---
+++ src/CoreShop/Bundle/CoreBundle/Migrations/Version20241018060845.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CoreShop\Bundle\CoreBundle\Migrations;
+
+use CoreShop\Bundle\FrontendBundle\CoreShopFrontendBundle;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+final class Version20241018060845 extends AbstractMigration implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function getDescription(): string
+    {
+        return 'Enable the CoreShop FrontendBundle in bundles.php';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->enableFrontendBundle();
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->disableFrontendBundle();
+    }
+
+    private function enableFrontendBundle(): void
+    {
+        $file = $this->getConfigFile();
+        $registered = $this->load($file);
+
+        $class = CoreShopFrontendBundle::class;
+        $bundle = ltrim($class, '\\');
+
+        if (!isset($bundles[$bundle])) {
+            $registered[$bundle] = ['all' => true];
+        }
+
+        $this->dump($file, $registered);
+    }
+
+    private function disableFrontendBundle(): void
+    {
+        $file = $this->getConfigFile();
+        $registered = $this->load($file);
+
+        $class = CoreShopFrontendBundle::class;
+        $bundle = ltrim($class, '\\');
+
+        if (isset($bundles[$bundle])) {
+            unset($registered[$bundle]);
+        }
+
+        $this->dump($file, $registered);
+    }
+
+    private function dump(string $file, array $bundles)
+    {
+        $contents = $this->buildContents($bundles);
+
+        if (!is_dir(\dirname($file))) {
+            if (!mkdir($concurrentDirectory = \dirname($file), 0777, true) && !is_dir($concurrentDirectory)) {
+                throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+            }
+        }
+
+        file_put_contents($file, $contents);
+
+        if (\function_exists('opcache_invalidate')) {
+            opcache_invalidate($file);
+        }
+    }
+
+    private function buildContents(array $bundles): string
+    {
+        $contents = "<?php\n\nreturn [\n";
+        foreach ($bundles as $class => $envs) {
+            $contents .= "    $class::class => [";
+            foreach ($envs as $env => $value) {
+                $booleanValue = var_export($value, true);
+                $contents .= "'$env' => $booleanValue, ";
+            }
+            $contents = substr($contents, 0, -2)."],\n";
+        }
+        $contents .= "];\n";
+
+        return $contents;
+    }
+
+    private function load(string $file): array
+    {
+        $bundles = file_exists($file) ? (require $file) : [];
+        if (!\is_array($bundles)) {
+            $bundles = [];
+        }
+
+        return $bundles;
+    }
+
+    private function getConfigFile(): string
+    {
+        return $this->container->getParameter('kernel.project_dir').'/config/bundles.php';
+    }
+}
