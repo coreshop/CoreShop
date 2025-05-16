c0a863bbeaaa3eb42692a76d5761da4284930e7c
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-05-16 12:20:41 +0200

    [Workflow] fix object type for payum

--- src/CoreShop/Bundle/PayumBundle/Action/Offline/ConfirmOrderAction.php
+++ src/CoreShop/Bundle/PayumBundle/Action/Offline/ConfirmOrderAction.php
@@ -20,6 +20,7 @@ namespace CoreShop\Bundle\PayumBundle\Action\Offline;
 
 use CoreShop\Bundle\PayumBundle\Request\ConfirmOrder;
 use CoreShop\Bundle\WorkflowBundle\Applier\StateMachineApplier;
+use CoreShop\Bundle\WorkflowBundle\Applier\StateMachineApplierInterface;
 use CoreShop\Component\Core\Model\PaymentInterface;
 use CoreShop\Component\Order\Model\OrderInterface;
 use CoreShop\Component\Order\OrderTransitions;
@@ -28,7 +29,7 @@ use Payum\Core\Action\ActionInterface;
 final class ConfirmOrderAction implements ActionInterface
 {
     public function __construct(
-        private StateMachineApplier $stateMachineApplier,
+        private StateMachineApplierInterface $stateMachineApplier,
     ) {
     }
 
