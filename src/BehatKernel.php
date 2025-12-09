7e3ea61b5d970aae94869130a59025efc7ea6504
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2025-12-09 13:58:23 +0100

    [Pimcore] symfony 7.x

--- src/BehatKernel.php
+++ src/BehatKernel.php
@@ -33,11 +33,4 @@ class BehatKernel extends PimcoreKernel
 
         \Pimcore::setKernel($this);
     }
-
-    protected function build(\Symfony\Component\DependencyInjection\ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->setParameter('pimcore.geoip.db_file', $container->getParameter('kernel.project_dir') . '/var/config/GeoLite2-City.mmdb');
-    }
 }
