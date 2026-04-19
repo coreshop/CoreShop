fdfa3953e3cfed34181ee5400738b9b61f77d95c
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-04-19 21:15:49 +0200

    vendor Pimcore GoogleMarketing tracker primitives into TrackingBundle (bundle dropped in Pimcore 2026.1)

--- src/CoreShop/Behat/Context/Domain/TrackingContext.php
+++ src/CoreShop/Behat/Context/Domain/TrackingContext.php
@@ -32,9 +32,9 @@ use CoreShop\Component\Core\Model\ProductInterface;
 use CoreShop\Component\Registry\ServiceRegistry;
 use CoreShop\Component\Tracking\Extractor\TrackingExtractorInterface;
 use CoreShop\Component\Tracking\Tracker\TrackerInterface;
-use Pimcore\Bundle\GoogleMarketingBundle\Code\CodeCollector;
-use Pimcore\Bundle\GoogleMarketingBundle\Tracker\AbstractTracker;
-use Pimcore\Bundle\GoogleMarketingBundle\Tracker\Tracker;
+use CoreShop\Bundle\TrackingBundle\GoogleMarketing\Code\CodeCollector;
+use CoreShop\Bundle\TrackingBundle\GoogleMarketing\Tracker\AbstractTracker;
+use CoreShop\Bundle\TrackingBundle\GoogleMarketing\Tracker\Tracker;
 use Webmozart\Assert\Assert;
 
 final class TrackingContext implements Context
