d70e2b68f7a17a05e1ec15dab3a7b347fdb03217
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-04-20 08:53:37 +0200

    add verifyRoute/verifyUrl helpers on SymfonyPage for native Symfony routing

--- src/CoreShop/Bundle/TestBundle/Page/SymfonyPage.php
+++ src/CoreShop/Bundle/TestBundle/Page/SymfonyPage.php
@@ -20,6 +20,7 @@ namespace CoreShop\Bundle\TestBundle\Page;
 use Behat\Mink\Element\NodeElement;
 use CoreShop\Bundle\TestBundle\Service\DriverHelper;
 use FriendsOfBehat\PageObjectExtension\Page\SymfonyPage as BaseSymfonyPage;
+use FriendsOfBehat\PageObjectExtension\Page\UnexpectedPageException;
 
 abstract class SymfonyPage extends BaseSymfonyPage implements SymfonyPageInterface
 {
@@ -28,5 +29,62 @@ abstract class SymfonyPage extends BaseSymfonyPage implements SymfonyPageInterfa
         DriverHelper::waitForPageToLoad($this->getSession());
 
         return parent::getElement($name, $parameters);
+    }
+
+    public function verifyRoute(array $requiredUrlParameters = []): void
+    {
+        $url = $this->getDriver()->getCurrentUrl();
+        $path = $this->stripFrontController(parse_url($url)['path']);
+
+        $matchedRoute = $this->router->match($path);
+
+        if ($matchedRoute['_route'] !== $this->getRouteName()) {
+            throw new UnexpectedPageException(sprintf(
+                "Matched route '%s' does not match the expected route '%s' for URL '%s'",
+                $matchedRoute['_route'],
+                $this->getRouteName(),
+                $url,
+            ));
+        }
+
+        foreach ($requiredUrlParameters as $key => $value) {
+            if (!isset($matchedRoute[$key]) || (string) $matchedRoute[$key] !== (string) $value) {
+                throw new UnexpectedPageException(sprintf(
+                    "Required route parameter '%s' with value '%s' not found in URL '%s'",
+                    $key,
+                    $value,
+                    $url,
+                ));
+            }
+        }
+    }
+
+    protected function verifyUrl(array $urlParameters = []): void
+    {
+        $url = $this->getDriver()->getCurrentUrl();
+        $path = $this->stripFrontController(parse_url($url)['path']);
+
+        $matchedRoute = $this->router->match($path);
+
+        if (isset($matchedRoute['_locale'])) {
+            $urlParameters += ['_locale' => $matchedRoute['_locale']];
+        }
+
+        if ($this->getSession()->getCurrentUrl() !== $this->getUrl($urlParameters)) {
+            throw new UnexpectedPageException(sprintf(
+                'Expected to be on "%s" but found "%s" instead',
+                $this->getUrl($urlParameters),
+                $this->getSession()->getCurrentUrl(),
+            ));
+        }
+    }
+
+    private function stripFrontController(string $path): string
+    {
+        return preg_replace(
+            '#^/(app(_dev|_test|_test_cached)?|index(_test|_test_precision)?)\.php/#',
+            '/',
+            $path,
+        );
     }
 }
