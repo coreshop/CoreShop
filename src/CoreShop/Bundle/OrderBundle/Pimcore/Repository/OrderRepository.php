baedf143c429e0f43ac1c023c976f7c9cd4bfb6c
Georg Wurz <wurz@cors.gmbh>
2026-06-16 09:57:00 +0200

    [OrderBundle] Restore last-opened cart across sessions

--- src/CoreShop/Bundle/OrderBundle/Pimcore/Repository/OrderRepository.php
+++ src/CoreShop/Bundle/OrderBundle/Pimcore/Repository/OrderRepository.php
@@ -34,7 +34,7 @@ class OrderRepository extends PimcoreRepository implements OrderRepositoryInterf
     public function findLatestByStoreAndCustomer(
         StoreInterface $store,
         CustomerInterface $customer,
-        string $name = null
+        string $name = null,
     ): ?StorageListInterface {
         return $this->findLatestCartByStoreAndCustomer($store, $customer);
     }
@@ -103,8 +103,9 @@ class OrderRepository extends PimcoreRepository implements OrderRepositoryInterf
     {
         $list = $this->getList();
         $list->setCondition('customer__id = ? AND store = ? AND saleState = ? ', [$customer->getId(), $store->getId(), OrderSaleStates::STATE_CART]);
-        $list->setOrderKey('creationDate');
-        $list->setOrder('DESC');
+        // Prefer the cart the customer last activated
+        $list->setOrderKey(['lastActivatedAt', 'creationDate']);
+        $list->setOrder(['DESC', 'DESC']);
         $list->load();
 
         $objects = $list->getObjects();
