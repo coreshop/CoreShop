5ae0ba82105e90f607940fef900694eeab2736db
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2026-06-17 10:09:00 +0200

    fix null/0 in SessionStorageListSubscriber

--- src/CoreShop/Bundle/StorageListBundle/Core/EventListener/SessionStoreStorageListSubscriber.php
+++ src/CoreShop/Bundle/StorageListBundle/Core/EventListener/SessionStoreStorageListSubscriber.php
@@ -79,7 +79,7 @@ final class SessionStoreStorageListSubscriber implements EventSubscriberInterfac
             return;
         }
 
-        if (0 !== $storageList->getId() && null !== $storageList->getStore()) {
+        if (0 !== $storageList->getId() && null !== $storageList->getId() && null !== $storageList->getStore()) {
             $this->storageListStorage->setForContext(['store' => $storageList->getStore()], $storageList);
         }
     }
