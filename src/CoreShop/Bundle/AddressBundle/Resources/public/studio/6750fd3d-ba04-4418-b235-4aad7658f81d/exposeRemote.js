b8a33fdf8f1bb16172156f1c77b30e0b05cf77e7
dpfaffenbauer <5981845+dpfaffenbauer@users.noreply.github.com>
2026-07-24 09:52:31 +0000

    Build CoreShop Studio bundles [skip ci]

---
+++ src/CoreShop/Bundle/AddressBundle/Resources/public/studio/6750fd3d-ba04-4418-b235-4aad7658f81d/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.coreshopaddress = "/bundles/coreshopaddress/studio/6750fd3d-ba04-4418-b235-4aad7658f81d/static/js/remoteEntry.js"
+
+      
+    
---
+++ src/CoreShop/Bundle/ProductQuantityPriceRulesBundle/Resources/public/studio/b3c97818-2753-4dac-8fe2-eda961057ec9/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.coreshopproductquantitypricerules = "/bundles/coreshopproductquantitypricerules/studio/b3c97818-2753-4dac-8fe2-eda961057ec9/static/js/remoteEntry.js"
+
+      
+    
